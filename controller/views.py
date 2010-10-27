5186d1ad0775e8d91f39c83110361a743f963b73
Alex Dorey <dorey415@gmail.com>
2010-10-27 16:28:37 -0400

    Dashboard template loaded

--- controller/views.py
+++ controller/views.py
@@ -1,1 +1,4 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def dashboard(request):
+    return render_to_response('dashboard.html')
--- urls.py
+++ urls.py
@@ -3,7 +3,12 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+from controller.views import dashboard
+from settings import MEDIA_ROOT
+
 urlpatterns = patterns('',
+    (r'^/?$', dashboard),
+
     (r'^storage/?',     include('nmis.django_eav.urls')),
     (r'^analysis/?',    include('nmis.nmis_analysis.urls')),
 
@@ -12,4 +17,7 @@ urlpatterns = patterns('',
     (r'^formList/?',    include('nmis.odk_dropbox.urls')),
 
     (r'^admin/', include(admin.site.urls)),
+    
+    #No static serve in PRODUCTION
+    (r'^site_media/(?P<path>.+)$', 'django.views.static.serve', {'document_root':MEDIA_ROOT}),
 )
