0784e5419ef4b5c2e9c44eecd37ce062bae0d443
Alex Dorey <dorey415@gmail.com>
2010-10-27 17:18:06 -0400

    fabfile updated

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,8 @@
-from fabric.api import run, settings
+from fabric.api import run, settings, local
+#from fabric.decorators import hosts
+#from fabric.operations import local
 
+#@hosts('localhost')
 def git_clone():
     "Clones the repositories"
     
@@ -7,18 +10,13 @@ def git_clone():
     nmis_analysis_repo = "git@github.com:mvpdev/nmis_analysis.git"
     django_eav_repo = "git@github.com:mvpdev/django-eav.git"
     
-    run("git clone %s odk_dropbox" % odk_repo)
-    run("git clone %s nmis_analysis" % nmis_analysis_repo)
-    run("git clone %s django_eav" % django_eav_repo)
+    local('git clone %s odk_dropbox' % odk_repo, capture=True)
+    local("git clone %s nmis_analysis" % nmis_analysis_repo, capture=True)
+    local("git clone %s django_eav" % django_eav_repo, capture=True)
 
-def git_pull():
+def git_pull_all():
     "Updates all the repositories"
     
-    with('cd odk_dropbox'):
-        run('git pull origin master')
-    
-    with('cd nmis_analysis'):
-        run('git pull origin master')
-    
-    with('cd django_eav'):
-        run('git pull origin master')
+    local('cd odk_dropbox && git pull origin master', capture=True)
+    local('cd nmis_analysis && git pull origin master', capture=True)
+    local('cd django_eav && git pull origin master', capture=True)
