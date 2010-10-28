58d80882433d105913a839b3b034b75d08b0bd2d
Andrew Marder <andrew.n.marder@gmail.com>
2010-10-28 17:00:42 -0400

    cleaned up project urls

--- urls.py
+++ urls.py
@@ -3,21 +3,15 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-from controller.views import dashboard
 from settings import MEDIA_ROOT
 
 urlpatterns = patterns('',
-    (r'^/?$', dashboard),
+    # odk dropbox urls
+    (r'', include('nmis.odk_dropbox.urls')),
 
-    (r'^storage/?',     include('nmis.django_eav.urls')),
-    (r'^analysis/?',    include('nmis.nmis_analysis.urls')),
-
-    (r'^dropbox/?',     include('nmis.odk_dropbox.urls')),
-    (r'^submission/?',  include('nmis.odk_dropbox.urls')),
-    (r'^formList/?',    include('nmis.odk_dropbox.urls')),
-
+    # admin urls
     (r'^admin/', include(admin.site.urls)),
     
-    #No static serve in PRODUCTION
-    (r'^site_media/(?P<path>.+)$', 'django.views.static.serve', {'document_root':MEDIA_ROOT}),
+    # static serve site media / only for development
+    (r'^site-media/(?P<path>.+)$', 'django.views.static.serve', {'document_root' : MEDIA_ROOT}),
 )
