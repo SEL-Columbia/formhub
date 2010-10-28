406f5725d936208c0a3b5510d9b8b5700218ae75
Andrew Marder <andrew.n.marder@gmail.com>
2010-10-28 19:13:24 -0400

    added controller to installed apps

--- settings.py
+++ settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    
+
+    'controller',
     'odk_dropbox',
     'nmis_analysis',
     'eav',
