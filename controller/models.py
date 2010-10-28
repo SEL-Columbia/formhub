b7a477038e78ee2d71886073f68fdc2b49c61e3b
Andrew Marder <andrew.n.marder@gmail.com>
2010-10-28 19:12:01 -0400

    got xml put into the eav

--- controller/models.py
+++ controller/models.py
@@ -6,6 +6,7 @@ from xml.sax import parseString
 
 from odk_dropbox.models import Submission
 from eav.models import Attribute, Value
+from eav.fields import EavSlugField
 
 class EAVHandler(ContentHandler):
 
@@ -21,32 +22,48 @@ class EAVHandler(ContentHandler):
         # Create a new eav.Value
         # the entity is the top of stack
         # the attribute is shares the name of this tag
-        a = Attribute.objects.get(slug=name)
-        o = Value(entity=self._stack.top(), attribute=a)
+        print "startElement:", name
+        slug = EavSlugField.create_slug_from_name(name)
+        try:
+            a = Attribute.objects.get(slug=slug)
+        except Attribute.DoesNotExist as e:
+            d = {"slug" : slug,
+                 "datatype" : Attribute.TYPE_TEXT,
+                 "name" : name,}
+            a = Attribute.objects.create(**d)
+        o = Value(entity=self._stack[-1], attribute=a)
+
+        # Unfortunately I have to save the object here so that
+        # subsequent objects can reference it.
+        o.save()
         self._stack.append(o)
         self._objects.append(o)
 
         # make sure there are no attributes
-        assert attrs.getLength()==0, "This parser is not equipped to handle attributes."
+        if attrs.getLength() > 0:
+            print "We're ignoring some attributes here."
 
     def characters(self, content):
         # ignore whitespace
         s = content.strip()
         if s:
             # set the value of the eav.Value on the top of the stack
-            self._stack.top().value = content
+            # and update it in the database
+            print "content:", content
+            self._stack[-1].value = content
+            self._stack[-1].save()
 
     def endElement(self, name):
+        slug = EavSlugField.create_slug_from_name(name)
+        print "endElement:", name
         top = self._stack.pop()
-        assert name==top.attribute.slug, "endElement doesn't match last startElement"
+        assert slug==top.attribute.slug, "endElement doesn't match last startElement"
 
 
 def add_to_eav(submission):
     handler = EAVHandler(submission)
-    parseString(submission.xml, handler)
-    # save all the objects created by parsing the submission's xml
-    for o in handler.get_objects:
-        o.save()
+    byte_string = submission.xml.encode( "utf-8" )
+    parseString(byte_string, handler)
 
 def _add_to_eav(sender, **kwargs):
     # if this is a new submission add it to the eav
