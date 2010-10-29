2f47246e65d3ed02e798e873575834c699404284
Alex Dorey <dorey415@gmail.com>
2010-10-29 09:32:12 -0400

    importing dashboard view and displaying in URLS (should not break admin this time...)

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 from settings import MEDIA_ROOT
+from controller.views import dashboard
 
 urlpatterns = patterns('',
     # odk dropbox urls
@@ -11,6 +12,10 @@ urlpatterns = patterns('',
 
     # admin urls
     (r'^admin/', include(admin.site.urls)),
+    
+    # Web UI:
+    # UI: Dashboard
+    url(r"^/?$", dashboard),
     
     # static serve site media / only for development
     (r'^site-media/(?P<path>.+)$', 'django.views.static.serve', {'document_root' : MEDIA_ROOT}),
