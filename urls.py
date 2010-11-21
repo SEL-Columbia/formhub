42187875842b799c8c3d0c8d3eab6521bd8cf55c
Andrew Marder <andrew.n.marder@gmail.com>
2010-11-21 18:46:34 -0500

    moved dashboard url into odk_dashboard.urls

--- urls.py
+++ urls.py
@@ -4,18 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 from settings import MEDIA_ROOT
-from odk_dashboard.views import dashboard
 
 urlpatterns = patterns('',
-    # odk dropbox urls
     (r'', include('nmis.odk_dropbox.urls')),
-
-    # admin urls
+    (r'', include('nmis.odk_dashboard.urls')),
     (r'^admin/', include(admin.site.urls)),
-    
-    # Web UI:
-    # UI: Dashboard
-    url(r"^/?$", dashboard),
     
     # static serve site media / only for development
     (r'^site-media/(?P<path>.+)$', 'django.views.static.serve', {'document_root' : MEDIA_ROOT}),
