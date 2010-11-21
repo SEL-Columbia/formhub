8947d8385ea939d55b1eb93941ea9106b8c4865a
Andrew Marder <andrew.n.marder@gmail.com>
2010-11-21 18:48:35 -0500

    added views for submission counts and csv export

--- odk_dashboard/urls.py
+++ odk_dashboard/urls.py
@@ -1,2 +1,11 @@
-#from django.conf.urls.defaults import *
-#urlpatterns = patterns()
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+
+from django.conf.urls.defaults import *
+from . import views
+
+urlpatterns = patterns('',
+    url(r"^/?$", views.dashboard),
+    url(r"^submission-counts$", views.submission_counts),
+    url(r"^(?P<survey_type>.*)\.csv$", views.csv),
+)
--- odk_dashboard/views.py
+++ odk_dashboard/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render_to_response
+from django.db.models import Avg, Max, Min, Count
+from django.http import HttpResponse
+from odk_dropbox import utils
+from .models import ParsedSubmission
 
 def dashboard(request):
     return render_to_response('dashboard.html')
+
+def submission_counts(request):
+    s = ParsedSubmission.objects.values("survey_type").annotate(Count("survey_type"))
+    return render_to_response("submission_counts.html",
+                              {"submission_counts" : s})
+
+def csv(request, survey_type):
+    pss = ParsedSubmission.objects.filter(survey_type=survey_type)
+    handlers = [utils.parse_submission(ps.submission) for ps in pss]
+    dicts = [handler.get_dict() for handler in handlers]
+    itemss = [utils.flatten_dict(d) for d in dicts]
+    flattened_dicts = [dict(items) for items in itemss]    
+    table = utils.table(flattened_dicts)
+    return HttpResponse(utils.csv(table), mimetype="application/csv")
