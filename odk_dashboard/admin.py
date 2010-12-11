94599bfac54c5d60ec111008fd869d247a86828d
Andrew Marder <andrew.n.marder@gmail.com>
2010-12-11 07:10:08 -0500

    added admin action to clean up parsed submissions

--- odk_dashboard/admin.py
+++ odk_dashboard/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from . import models
+from odk_dropbox.models import Submission
 
 admin.site.register(models.Phone)
 admin.site.register(models.GPS)
 admin.site.register(models.SurveyType)
-admin.site.register(models.ParsedSubmission)
 admin.site.register(models.Surveyor)
+
+def reparse_all(modeladmin, request, queryset):
+    models.ParsedSubmission.objects.all().delete()
+    for s in Submission.objects.all():
+        models.parse(s)
+reparse_all.short_description = "Delete and recreate all parsed submissions."
+
+class ParsedSubmissionAdmin(admin.ModelAdmin):
+    actions = [reparse_all]
+
+admin.site.register(models.ParsedSubmission, ParsedSubmissionAdmin)
