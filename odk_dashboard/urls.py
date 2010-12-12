07777d1099027928c2249d10642c61d4ce67e74e
Alex Dorey <dorey415@gmail.com>
2010-12-11 21:55:37 -0500

    Adding redirect to avoid confusion with old links.

--- odk_dashboard/urls.py
+++ odk_dashboard/urls.py
@@ -2,11 +2,14 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 from django.conf.urls.defaults import *
+from django.views.generic.simple import redirect_to
+
 from . import views
 
 urlpatterns = patterns('',
     url(r"^/?$", views.dashboard),
     url(r"^(?P<name>.*)\.csv$", views.csv),
+    url(r"^map/?", redirect_to, {'url': '/view'}),
     #4 main sections:
     url(r"^profiles/?$", views.profiles_section),
     url(r"^data/?$", views.submission_counts),
