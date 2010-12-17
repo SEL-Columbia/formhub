f37a3d78fc8b660ad8d97be52871e8796a4d5e52
Andrew Marder <andrew.n.marder@gmail.com>
2010-12-17 17:25:02 -0500

    moved import view into a management command

---
+++ odk_dropbox/management/commands/import.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+
+import os, glob
+from django.core.management.base import BaseCommand
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from ...models import make_submission, InstanceImage
+
+def django_file(path, field_name, content_type):
+    # adapted from here: http://groups.google.com/group/django-users/browse_thread/thread/834f988876ff3c45/
+    f = open(path)
+    return InMemoryUploadedFile(
+        file=f,
+        field_name=field_name,
+        name=f.name,
+        content_type=content_type,
+        size=os.path.getsize(path),
+        charset=None
+        )
+
+class Command(BaseCommand):
+    help = "Import a folder of ODK instances."
+
+    def handle(self, *args, **kwargs):
+        path = args[0]
+        for instance in glob.glob( os.path.join(path, "*") ):
+            xml_files = glob.glob( os.path.join(instance, "*.xml") )
+            if len(xml_files)<1: continue
+            if len(xml_files)>1: raise Exception(
+                "Figure out how to handle multiple XML files",
+                xml_files
+                )
+            print xml_files[0]
+            f = django_file(xml_files[0],
+                            field_name="xml_file",
+                            content_type="text/xml")
+            s = make_submission(f)
+
+            for jpg in glob.glob( os.path.join(instance, "*.jpg") ):
+                print jpg
+                f = django_file(jpg,
+                                field_name="image",
+                                content_type="image/jpeg")
+                InstanceImage.objects.get_or_create(
+                    instance=s.instance, image=f
+                    )
--- odk_dropbox/views.py
+++ odk_dropbox/views.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 
-import os, glob
 from django.views.decorators.http import require_GET, require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
-from .models import Form, Submission, SubmissionImage
+from .models import Form, InstanceImage, make_submission
 
 @require_GET
 def formList(request):
@@ -22,46 +21,22 @@ def submission(request):
     # request.FILES is a django.utils.datastructures.MultiValueDict
     # for each key we have a list of values
     xml_file_list = request.FILES.pop("xml_submission_file")
-    assert len(xml_file_list)==1, "There should be a single xml file in this submission."
-    s = Submission(xml_file=xml_file_list[0])
-    s.save()
+    assert len(xml_file_list)==1, \
+        "There should be a single xml file in this submission."
+    s = make_submission(xml_file_list[0])
 
     # save the rest of the files to the filesystem
     # these should all be images
     for key in request.FILES.keys():
         for image in request.FILES.getlist(key):
-            SubmissionImage.objects.create(submission=s, image=image)
+            InstanceImage.objects.create(
+                instance=s.instance, image=image
+                )
 
     # ODK needs two things for a form to be considered successful
     # 1) the status code needs to be 201 (created)
     # 2) The location header needs to be set to the host it posted to
-    response = HttpResponse("You've successfully made an odk submission.")
+    response = HttpResponse("Your ODK submission was successful.")
     response.status_code = 201
     response['Location'] = "http://%s/submission" % request.get_host()
     return response
-
-def import_instances_folder(path):
-    for instance in glob.glob( os.path.join(path, "*") ):
-        xml_files = glob.glob( os.path.join(instance, "*.xml") )
-        if len(xml_files)!=1: continue
-        print xml_files[0]
-        f = django_file(xml_files[0], field_name="xml_file", content_type="text/xml")
-        s = Submission(xml_file=f)
-        s.save()
-
-        for jpg in glob.glob( os.path.join(instance, "*.jpg") ):
-            print jpg
-            f = django_file(jpg, field_name="image", content_type="image/jpeg")
-            SubmissionImage.objects.create(submission=s, image=f)
-
-def django_file(path, field_name, content_type):
-    # adapted from here: http://groups.google.com/group/django-users/browse_thread/thread/834f988876ff3c45/
-    from django.core.files.uploadedfile import InMemoryUploadedFile
-    f = open(path)
-    return InMemoryUploadedFile(
-        file=f,
-        field_name=field_name,
-        name=f.name,
-        content_type=content_type,
-        size=os.path.getsize(path),
-        charset=None)
