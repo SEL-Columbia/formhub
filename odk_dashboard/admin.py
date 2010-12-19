cccb2b0b3b9fbb95e9376554b43d69f90249fe60
Andrew Marder <andrew.n.marder@gmail.com>
2010-12-19 14:47:31 -0500

    renamed submission to instance

--- odk_dashboard/admin.py
+++ odk_dashboard/admin.py
@@ -5,15 +5,16 @@ from odk_dropbox.models import Submission
 admin.site.register(models.Phone)
 admin.site.register(models.GPS)
 admin.site.register(models.SurveyType)
+admin.site.register(models.Location)
 admin.site.register(models.Surveyor)
 
 def reparse_all(modeladmin, request, queryset):
-    models.ParsedSubmission.objects.all().delete()
-    for s in Submission.objects.all():
+    models.ParsedInstance.objects.all().delete()
+    for s in Instance.objects.all():
         models.parse(s)
-reparse_all.short_description = "Delete and recreate all parsed submissions."
+reparse_all.short_description = "Delete and recreate all parsed instances."
 
-class ParsedSubmissionAdmin(admin.ModelAdmin):
+class ParsedInstanceAdmin(admin.ModelAdmin):
     actions = [reparse_all]
 
-admin.site.register(models.ParsedSubmission, ParsedSubmissionAdmin)
+admin.site.register(models.ParsedInstance, ParsedInstanceAdmin)
