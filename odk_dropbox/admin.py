9632e8af8d9bd8e81bdd5d9ae818225ef00da39e
Andrew Marder <andrew.n.marder@gmail.com>
2010-12-19 14:48:18 -0500

    renamed submission to instance and added submission model.

--- odk_dropbox/admin.py
+++ odk_dropbox/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Form)
+admin.site.register(models.Instance)
+admin.site.register(models.InstanceImage)
 admin.site.register(models.Submission)
-admin.site.register(models.SubmissionImage)
