283b580ef8314c7af975a691ed3661241e894286
Andrew Marder <andrew.n.marder@gmail.com>
2010-12-19 23:30:20 -0500

    moved reparsing out of admin into management command

--- odk_dashboard/admin.py
+++ odk_dashboard/admin.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
 from . import models
-from odk_dropbox.models import Submission
 
 admin.site.register(models.Phone)
 admin.site.register(models.GPS)
 admin.site.register(models.SurveyType)
 admin.site.register(models.Location)
+admin.site.register(models.ParsedInstance)
 admin.site.register(models.Surveyor)
-
-def reparse_all(modeladmin, request, queryset):
-    models.ParsedInstance.objects.all().delete()
-    for s in Instance.objects.all():
-        models.parse(s)
-reparse_all.short_description = "Delete and recreate all parsed instances."
-
-class ParsedInstanceAdmin(admin.ModelAdmin):
-    actions = [reparse_all]
-
-admin.site.register(models.ParsedInstance, ParsedInstanceAdmin)
---
+++ odk_dashboard/management/commands/reparse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+
+from django.core.management.base import BaseCommand
+from odk_dashboard import models
+from odk_dropbox.models import Instance
+
+class Command(BaseCommand):
+    help = "Delete and recreate all parsed instances."
+
+    def handle(self, *args, **kwargs):
+        models.ParsedInstance.objects.all().delete()
+        for i in Instance.objects.all():
+            print i.xml_file.name
+            models.parse(i)
