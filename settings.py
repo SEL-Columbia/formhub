48b112e11803fc692049bc4390d983fa465972ba
Andrew Marder <andrew.n.marder@gmail.com>
2010-12-21 22:46:22 -0500

    added django-mako to middleware

--- settings.py
+++ settings.py
@@ -23,6 +23,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'djangomako.middleware.MakoMiddleware',
 )
 
 ROOT_URLCONF = 'nmis.urls'
