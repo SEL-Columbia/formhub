5dadf7a1edd07aa78561e0b2665ee5dc208a244b
Alex Dorey <dorey415@gmail.com>
2011-01-14 11:32:08 -0500

    Changing name from downloads to data.

--- site_media/js/nmis.dashboard.sammy.js
+++ site_media/js/nmis.dashboard.sammy.js
@@ -574,7 +574,7 @@ var zz;
 var dashboard = (function($){
     $(function(){
         var menu = $('#menu .fwidth').empty();
-        menu.append($('<li />').html($("<a />", {href:"#/"}).html("Downloads"))); //why is this not loading in the page?
+        menu.append($('<li />').html($("<a />", {href:"#/"}).html("Data"))); //why is this not loading in the page?
         menu.append($('<li />').html($("<a />", {href:"#/activity"}).html("Activity")))
         menu.append($('<li />').html($("<a />", {href:"#/frequency-tables"}).html("Frequency Tables")))
         menu.append($('<li />').html($("<a />", {href:"#/map"}).html("Map")))
@@ -595,7 +595,7 @@ var dashboard = (function($){
         this.use(Sammy.Template);
         
         this.get("#/", function(context){
-            var dashbElem = this.switchTo("dashboard", {title: "Downloads"});
+            var dashbElem = this.switchTo("dashboard", {title: "Data"});
             $.get("/survey-list", function(htResponse){
                 var surveyList = $(htResponse);
                 $('a', surveyList).button();
