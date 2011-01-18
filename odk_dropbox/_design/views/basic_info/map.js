dd8d5a41c201775d03a53803edff79567a28a665
Alex Dorey <dorey415@gmail.com>
2011-01-18 17:22:11 -0500

    cleaner js in activity-list's map.js

--- odk_dropbox/_design/views/basic_info/map.js
+++ odk_dropbox/_design/views/basic_info/map.js
@@ -1,12 +1,12 @@
 function(doc) {
-  var results = {};
-  (function(doc){
-    this.survey_type = doc.parsed_xml.survey_type;
-    this.image_url = doc.parsed_xml.survey_data.picture;
-    this.district_id = null;
-    var geopoint = doc.parsed_xml.survey_data.geopoint.split(" ");
-    this.gps = {lat: geopoint[0], lng: geopoint[1]};
-    this.device_id = doc.parsed_xml.survey_data.device_id;
-  }).call(results, doc)
-  emit(doc._id, results);
+    emit(doc._id, (function(data){
+        var geopoint = data.survey_data.geopoint.split(" ");
+        return {
+            survey_type: data.survey_type,
+            image_url: data.survey_data.picture,
+            district_id: null,
+            gps: {lat: geopoint[0], lng: geopoint[1]},
+            device_id: data.survey_data.device_id
+        }
+    })(doc.parsed_xml));
 }
