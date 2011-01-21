cd28825b8634f45321d72c2c2795425b13bf038e
Andrew Marder <andrew.ei.marder@gmail.com>
2011-01-21 17:41:43 -0500

    got rid of odk_dashboard

--- odk_dashboard/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# admin.site.register(models.Phone)
-# admin.site.register(models.GPS)
-# admin.site.register(models.SurveyType)
-# admin.site.register(models.Location)
-# admin.site.register(models.ParsedInstance)
-# admin.site.register(models.Surveyor)
