b79e0d726b0d2300c14377f6e3701006f1f844d5
Andrew Marder <andrew.ei.marder@gmail.com>
2011-01-25 14:24:58 -0500

    monster commit

--- odk_dropbox/tag.py
+++ odk_dropbox/tag.py
@@ -1,10 +1,14 @@
 # These are common variable tags that we'll want to access
-FORM_ID = u"_form_id"
-SURVEY_TYPE = u"_survey_type"
-TIME_START = u"start"
-TIME_END = u"end"
+XFORM_ID_STRING = u"_xform_id_string"
+INSTANCE_DOC_NAME = u"_instance_doc_name"
+ID = u"_id"
+DATE_TIME_START = u"start"
+DATE_TIME_END = u"end"
 DEVICE_ID = u"device_id"
 GPS = u"geopoint"
 LGA = u"lga"
-INSTANCE_ID = u"_id"
-DISTRICT_ID = u"_district_id"
+
+# value of INSTANCE_DOC_NAME that indicates a regisration form
+REGISTRATION = u"registration"
+# keys that we'll look for in the registration form
+REGISTRATION_NAME = u"name"
