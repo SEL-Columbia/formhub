2e23f0fd727d903f917ea40c0b06ecebc6676e9c
Alex Dorey <dorey415@gmail.com>
2011-01-26 14:54:26 -0500

    added test for out-of-order submissions.

--- odk_dropbox/tests/surveyor_registration.py
+++ odk_dropbox/tests/surveyor_registration.py
@@ -30,10 +30,10 @@ class TestSurveyorRegistration(TestCase):
     def test_multiple_registrations_on_the_same_phone(self):
         """
         Two users registered to phone '12345'.
-            1: Betty (2 days ago)
-            2: Alex (1 day ago)
+            1: Betty (hour 1)
+            2: Alex (hour 2)
         One submission:
-            1. WaterSimple (1 hour ago)
+            1. WaterSimple (hour 3)
 
         Submission should be attributed to "Alex Adams"
         """
@@ -41,27 +41,74 @@ class TestSurveyorRegistration(TestCase):
         water =  load_xform_from_file("odk_dropbox/fixtures/test_forms/water_simple/forms/WaterSimple.xml")
         
         now = datetime.now()
-        one_day_ago = datetime(now.year, now.month, now.day-1)
-        two_days_ago = datetime(now.year, now.month, now.day-2)
+        ordered_times = [datetime(now.year, now.month, now.day, 1), \
+                        datetime(now.year, now.month, now.day, 2), \
+                        datetime(now.year, now.month, now.day, 3)]
         
         first_registration = load_registration_with_values({'form_id': xf.id_string, \
-            'start_time': two_days_ago, 'name': 'Betty', 'sex': 'female', \
+            'start_time': ordered_times[0], 'name': 'Betty Bimbob', 'sex': 'female', \
             'birth_date': '1970-07-07', 'device_id': '12345'})
         first_registration.save()
         
         second_registration = load_registration_with_values({'form_id':xf.id_string, \
-            'start_time': one_day_ago, \
+            'start_time': ordered_times[1], \
             'name': 'Alex Adams', 'birth_date': '1986-08-15', \
             'device_id': '12345'})
         second_registration.save()
 
         self.assertTrue(Surveyor.objects.count(), 2)
         
-        submission = load_simple_submission({'start_time': now})
+        submission = load_simple_submission({'start_time': ordered_times[2]})
         submission.save()
 
         self.assertTrue(submission.surveyor is not None)
         self.assertEqual(submission.surveyor.name(), 'Alex Adams')
+        
+    def test_multiple_submissions_out_of_order(self):
+        """
+        Two users registered to phone '12345'.
+        User    Submission
+        --      --
+        1: user_one (named Betty, hour 1)
+                2. submission_one # hour 2 - should be attributed to betty 
+        3: user_two (named Alex, hour 3)
+                4. submission_two # hour 4 - should be attributed to alex
+                
+        Registrations performed in order,
+        Submissions entered out of order.
+        """
+        xf = load_xform_from_file("odk_dropbox/fixtures/test_forms/registration/forms/test_registration.xml")
+        water =  load_xform_from_file("odk_dropbox/fixtures/test_forms/water_simple/forms/WaterSimple.xml")
+
+        now = datetime.now()
+        ordered_times = [datetime(now.year, now.month, now.day, 1), \
+                        datetime(now.year, now.month, now.day, 2), \
+                        datetime(now.year, now.month, now.day, 3), \
+                        datetime(now.year, now.month, now.day, 4)]
+        
+        user_one = load_registration_with_values({'form_id': xf.id_string, \
+            'start_time': ordered_times[0], 'name': 'Betty Bimbob', 'sex': 'female', \
+            'birth_date': '1970-07-07', 'device_id': '12345'})
+        user_one.save()
+
+        user_two = load_registration_with_values({'form_id':xf.id_string, \
+            'start_time': ordered_times[2], \
+            'name': 'Alex Adams', 'birth_date': '1986-08-15', \
+            'device_id': '12345'})
+        user_two.save()
+
+        self.assertTrue(Surveyor.objects.count(), 2)
+
+
+        # submissions are sometimes parsed out of order, so we are saving the 2nd submission first
+        submission_two = load_simple_submission({'start_time': ordered_times[3]})
+        submission_two.save()
+        
+        submission_one = load_simple_submission({'start_time': ordered_times[1]})
+        submission_one.save()
+
+        self.assertEqual(submission_one.surveyor.name(), 'Betty Bimbob')
+        self.assertEqual(submission_two.surveyor.name(), 'Alex Adams')
 
 """
 fix everything below this line some day
