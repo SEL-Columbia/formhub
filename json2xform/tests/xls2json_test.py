d9b7425bd3fcc76b13e1894d03b2ae0ec7b24cab
Alex Dorey <dorey415@gmail.com>
2011-01-27 21:30:34 -0500

    added 2 more tests

--- json2xform/tests/xls2json_test.py
+++ json2xform/tests/xls2json_test.py
@@ -6,14 +6,10 @@ import sys, os
 sys.path.append('/Users/alexdorey/Code/NMIS/mongo/nmis')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-
 from django.test import TestCase, Client
-
-import json
-
 from json2xform.xls2json import XlsForm
 
-class ConvertXls2JSON(TestCase):
+class BasicXls2JsonApiTests(TestCase):
 
     def test_simple_yes_or_no_question(self):
         x = XlsForm("json2xform/surveys/super_simple/yes_or_no_question.xls")
@@ -25,3 +21,22 @@ class ConvertXls2JSON(TestCase):
                          u'text': {u'english': u'have you had a good day today?'}}]
         
         self.assertEqual(x_results, expected_dict)
+
+
+    def test_gps(self):
+        x = XlsForm("json2xform/surveys/super_simple/gps.xls")
+
+        expected_dict = [{u'type': u'gps', u'name': u'location'}]
+
+        self.assertEqual(x.to_dict(), expected_dict)
+
+    
+    def test_string_and_integer(self):
+        x = XlsForm("json2xform/surveys/super_simple/string_and_integer.xls")
+
+        expected_dict = [{'text': {u'english': u'What is your name?'}, \
+                            u'type': u'string', u'name': u'your_name'}, \
+                        {'text': {u'english': u'How many years old are you?'}, \
+                            u'type': u'integer', u'name': u'your_age'}]
+
+        self.assertEqual(x.to_dict(), expected_dict)
