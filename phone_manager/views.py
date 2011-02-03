16537dbf72987566b73076bc16ac0a6f62cbf5c0
Alex Dorey <dorey415@gmail.com>
2011-02-03 11:10:25 -0500

    Added sample JSON. processing in browser and pushing to a table.

--- phone_manager/views.py
+++ phone_manager/views.py
@@ -7,4 +7,15 @@ def phone_manager(request):
     return render_to_response("phone_manager.html", info)
 
 def phone_manager_json(request):
-    return HttpResponse(simplejson.dumps({'message':'hello'}))
+    phonet = {}
+    phonet['columns'] = [{'name':'Registered To', 'id':'surveyor'}, \
+                    {'name':'Status','id':'status'}, {'name':'Notes','id':'notes'}, \
+                    {'name':'Visible ID','id':'external_id'}, {'name':'Current Number','id':'phone_number'}]
+                    
+    phonet['rows'] = [
+    {'surveyor':'Bob', 'status':'functional','notes':None, 'external_id':'001', 'phone_number':'609.902.4682'}, \
+    {'surveyor':'Sven', 'status':'functional','notes':None, 'external_id':'023', 'phone_number':'609.902.4681'}, \
+    {'surveyor':'Wynonah', 'status':'functional','notes':None, 'external_id':'024', 'phone_number':'609.902.4683'}, \
+    {'surveyor':None, 'status':'broken','notes':'GPS Broken', 'external_id':'007', 'phone_number':'609.902.4685'} \
+    ]
+    return HttpResponse(simplejson.dumps(phonet))
