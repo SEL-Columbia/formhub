cd7be73ff58f483bc4b91caa922cda77ab37dea4
Andrew Marder <andrew.ei.marder@gmail.com>
2011-02-03 16:40:49 -0500

    made xls2json more object oriented and fixed tests

--- json2xform/tests/xls2json_test.py
+++ json2xform/tests/xls2json_test.py
@@ -6,36 +6,44 @@ import sys, os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from django.test import TestCase, Client
-from json2xform.xls2json import XlsForm
+from json2xform.xls2json import ExcelToJsonConverter
 
 class BasicXls2JsonApiTests(TestCase):
 
     def test_simple_yes_or_no_question(self):
-        x = XlsForm("json2xform/surveys/super_simple/yes_or_no_question.xls")
+        x = ExcelToJsonConverter("json2xform/surveys/super_simple/yes_or_no_question.xls")
         x_results = x.to_dict()
         
-        expected_dict = [{u'choices': [{u'text': {u'english': u'yes'}, u'value': u'yes'},
-                      {u'text': {u'english': u'no'}, u'value': u'no'}],
-                         u'name': u'good_day', u'type': u'select one',
-                         u'text': {u'english': u'have you had a good day today?'}}]
+        expected_dict = [
+            {
+                u'choices': [
+                    {u'text': {u'english': u'yes'}, u'value': u'yes'},
+                    {u'text': {u'english': u'no'}, u'value': u'no'}
+                    ],
+                u'name': u'good_day',
+                u'type': u'select one',
+                u'text': {u'english': u'have you had a good day today?'},
+                u'attributes': {},
+                }
+            ]
         
         self.assertEqual(x_results, expected_dict)
 
 
     def test_gps(self):
-        x = XlsForm("json2xform/surveys/super_simple/gps.xls")
+        x = ExcelToJsonConverter("json2xform/surveys/super_simple/gps.xls")
 
-        expected_dict = [{u'type': u'gps', u'name': u'location'}]
+        expected_dict = [{'attributes': {}, u'type': u'gps', u'name': u'location'}]
 
         self.assertEqual(x.to_dict(), expected_dict)
 
     
     def test_string_and_integer(self):
-        x = XlsForm("json2xform/surveys/super_simple/string_and_integer.xls")
+        x = ExcelToJsonConverter("json2xform/surveys/super_simple/string_and_integer.xls")
 
-        expected_dict = [{'text': {u'english': u'What is your name?'}, \
-                            u'type': u'string', u'name': u'your_name'}, \
-                        {'text': {u'english': u'How many years old are you?'}, \
-                            u'type': u'integer', u'name': u'your_age'}]
+        expected_dict = [{u'text': {u'english': u'What is your name?'}, \
+                            u'type': u'string', u'name': u'your_name', u'attributes': {}}, \
+                        {u'text': {u'english': u'How many years old are you?'}, \
+                            u'type': u'integer', u'name': u'your_age', u'attributes': {}}]
 
         self.assertEqual(x.to_dict(), expected_dict)
--- json2xform/xls2json.py
+++ json2xform/xls2json.py
@@ -6,111 +6,110 @@ from xlrd import open_workbook
 import json
 import sys
 
-SURVEY = "survey"
-CHOICES = "choices"
-CHOICE_LIST_NAME = "list name"
-TEXT = "text"
-TEXT_PREFACE = TEXT + ":"
-TYPE = "type"
-RESERVED_FIELDS = ["name", "text", "hint", "type", "choices", "attributes"]
-ATTRIBUTES = "attributes"
+SURVEY = u"survey"
+CHOICES = u"choices"
+CHOICE_LIST_NAME = u"list name"
+TEXT = u"text"
+TEXT_PREFACE = TEXT + u":"
+TYPE = u"type"
+RESERVED_FIELDS = [u"name", u"text", u"hint", u"type", u"choices", u"attributes"]
+ATTRIBUTES = u"attributes"
 
-def _step1(path):
-    """
-    Return
-    {
-    "sheet1.name" : [
-    {col1.header : value[2,1], col2.header : value[2,2]}, ...
-    ]
-    "sheet2.name" : [ ...
-    ]
-    }
-    """
-    workbook = open_workbook(path)
-    result = {}
-    for sheet in workbook.sheets():
-        result[sheet.name] = []
-        for row in range(1,sheet.nrows):
-            row_dict = {}
-            for column in range(0,sheet.ncols):
-                key = sheet.cell(0,column).value
-                value = sheet.cell(row,column).value
-                if value:
-                    row_dict[key] = value
-            if row_dict: result[sheet.name].append(row_dict)
-    return result
-
-def _clean_text(pyobj):
-    for dicts in pyobj.values():
-        for d in dicts:
-            text = {}
-            for k, v in d.items():
-                if k.startswith(TEXT_PREFACE):
-                    text[k[len(TEXT_PREFACE):]] = v
-                    del d[k]
-            assert TEXT not in d
-            if text: d[TEXT] = text
-
-def _clean_choice_lists(pyobj):
-    choice_list = pyobj[CHOICES]
-    choices = {}
-    for choice in choice_list:
-        list_name = choice.pop(CHOICE_LIST_NAME)
-        if list_name in choices: choices[list_name].append(choice)
-        else: choices[list_name] = [choice]
-    pyobj[CHOICES] = choices
-
-def _fix_int_values(pyobj):
-    for choice_list in pyobj[CHOICES].values():
-        for choice in choice_list:
-            v = choice["value"]
-            if type(v)==float and v==int(v):
-                choice["value"] = int(v)
-
-def _insert_choice_lists(pyobj):
-    survey = pyobj[SURVEY]
-    for i in range(len(survey)):
-        if survey[i][TYPE].startswith("select"):
-            q_type, list_name = survey[i][TYPE].split("from")
-            survey[i][TYPE] = q_type.strip()
-            assert "choices" not in survey[i]
-            survey[i]["choices"] = pyobj[CHOICES][list_name.strip()]
-
-def _group_extra_attributes(pyobj):
-    survey = pyobj[SURVEY]
-    for q in survey:
-        assert ATTRIBUTES not in q
-        q[ATTRIBUTES] = {}
-        for k in q.keys():
-            if k not in RESERVED_FIELDS:
-                q[ATTRIBUTES][k] = q[k]
-                del q[k]
-
-def xls2json(path):
-    pyobj = _step1(path)
-    _clean_text(pyobj)
-    _clean_choice_lists(pyobj)
-    _fix_int_values(pyobj)
-    _insert_choice_lists(pyobj)
-    _group_extra_attributes(pyobj)
-    return json.dumps(pyobj[SURVEY], indent=4)
-
-if __name__ == '__main__':
-    print xls2json(sys.argv[1])
-
-
-class XlsForm(object):
+class ExcelToJsonConverter(object):
     def __init__(self, path):
-        self.dict = None
-        self.pyobj = _step1(path)
-    
+        self._path = path
+        self._pyobj = None
+
+        self._step1()
+        self._clean_text()
+        self._clean_choice_lists()
+        self._fix_int_values()
+        self._insert_choice_lists()
+        self._group_extra_attributes()
+
     def to_dict(self):
-        if self.dict is None:
-            _clean_text(self.pyobj)
-            _clean_choice_lists(self.pyobj)
-            _insert_choice_lists(self.pyobj)
-            self.dict = self.pyobj[SURVEY]
-        return self.dict
-    
-    def to_json(self):
-        return json.dumps(self.to_dict(), indent=4)
+        return self._pyobj
+
+    def get_json(self):
+        return json.dumps(self._pyobj, indent=4)
+
+    def _step1(self):
+        """
+        Return
+        {
+        "sheet1.name" : [
+        {col1.header : value[2,1], col2.header : value[2,2]}, ...
+        ]
+        "sheet2.name" : [ ...
+        ]
+        }
+        """
+        workbook = open_workbook(self._path)
+        self._pyobj = {}
+        for sheet in workbook.sheets():
+            self._pyobj[sheet.name] = []
+            for row in range(1,sheet.nrows):
+                row_dict = {}
+                for column in range(0,sheet.ncols):
+                    key = sheet.cell(0,column).value
+                    value = sheet.cell(row,column).value
+                    if value:
+                        row_dict[key] = value
+                if row_dict: self._pyobj[sheet.name].append(row_dict)
+
+    def _clean_text(self):
+        for dicts in self._pyobj.values():
+            for d in dicts:
+                text = {}
+                for k, v in d.items():
+                    if k.startswith(TEXT_PREFACE):
+                        text[k[len(TEXT_PREFACE):]] = v
+                        del d[k]
+                assert TEXT not in d
+                if text: d[TEXT] = text
+
+    def _clean_choice_lists(self):
+        choice_list = self._pyobj[CHOICES]
+        choices = {}
+        for choice in choice_list:
+            list_name = choice.pop(CHOICE_LIST_NAME)
+            if list_name in choices: choices[list_name].append(choice)
+            else: choices[list_name] = [choice]
+        self._pyobj[CHOICES] = choices
+
+    def _fix_int_values(self):
+        for choice_list in self._pyobj[CHOICES].values():
+            for choice in choice_list:
+                v = choice[u"value"]
+                if type(v)==float and v==int(v):
+                    choice[u"value"] = int(v)
+
+    def _insert_choice_lists(self):
+        survey = self._pyobj[SURVEY]
+        for i in range(len(survey)):
+            if survey[i][TYPE].startswith(u"select"):
+                q_type, list_name = survey[i][TYPE].split(u"from")
+                survey[i][TYPE] = q_type.strip()
+                assert u"choices" not in survey[i]
+                survey[i][u"choices"] = self._pyobj[CHOICES][list_name.strip()]
+        self._pyobj = survey
+
+    def _group_extra_attributes(self):
+        for q in self._pyobj:
+            assert ATTRIBUTES not in q
+            q[ATTRIBUTES] = {}
+            for k in q.keys():
+                if k not in RESERVED_FIELDS:
+                    q[ATTRIBUTES][k] = q[k]
+                    del q[k]
+
+if __name__=="__main__":
+    # Open the excel file that is the second argument to this python
+    # call, convert that file to json and save that json to a file
+    path = sys.argv[1]
+    assert path[-4:]==".xls"
+    converter = ExcelToJsonConverter(path)
+    print converter.get_json()
+    # f = open(path[:-4] + ".json", "w")
+    # f.write(converter.get_json())
+    # f.close()
