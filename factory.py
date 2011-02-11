4bb2161d93fb142f48bfaec3fdb5ca46561fd140
Alex Dorey <dorey415@gmail.com>
2011-02-11 08:38:10 -0500

    Added basic factories for phones and surveyors (might need to work a relationship in to these factory functions)

---
+++ factory.py
@@ -0,0 +1,6 @@
+#factories can be git-downloaded and installed from here:
+#https://github.com/mirhampt/django-factories
+
+from surveyor_manager.factory import SurveyorManagerFactory
+from phone_manager.factory import PhoneManagerFactory
+from locations.factory import DistrictFactory
