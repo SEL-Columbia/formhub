edf79c256fe919b26183ceb065084ca1e6296093
Alex Dorey <dorey415@gmail.com>
2011-02-16 13:38:59 -0500

    changed how expected_xml is passed to the assertion. tests pass.

--- json2xform/tests/j2x_question_tests.py
+++ json2xform/tests/j2x_question_tests.py
@@ -7,6 +7,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from django.test import TestCase, Client
 from .json2xform import *
+from ..question import create_question_from_dict
 
 import json
 
@@ -33,9 +34,11 @@ class Json2XformQuestionValidationTests(TestCase):
                     "type": "string", "name": "enumerator_name"}
 
         q = create_question_from_dict(simple_string_json)
+        expected_string_question_xml = """
+        <input ref="/test/enumerator_name"><label ref="jr:itext('q_enumerator_name')"/></input>
+        """
         self.s._add_element(q)
-        self.assertEqual(ctw(q.control()), \
-                "<input ref=\"/test/enumerator_name\"><label ref=\"jr:itext('q_enumerator_name')\"/></input>")
+        self.assertEqual(ctw(q.control()), expected_string_question_xml.strip())
     
     def test_select_one_question_multilingual(self):
         """
@@ -46,9 +49,13 @@ class Json2XformQuestionValidationTests(TestCase):
                 [{"text": {"f": "fa","e": "ea"},"value": "a"}, \
                 {"text": {"f": "fb","e": "eb"},"value": "b"}]}
 
+        expected_select_one_question_xml = """
+        <select1 ref="/test/qname"><label ref="jr:itext('q_qname')"/></select1>
+        """
+
         q = create_question_from_dict(simple_select_one_json)
         self.s._add_element(q)
-        self.assertEqual(ctw(q.control()), '<select1 ref="/test/qname"><label ref="jr:itext(\'q_qname\')"/></select1>')
+        self.assertEqual(ctw(q.control()), expected_select_one_question_xml.strip())
 
     def test_simple_integer_question_type_multilingual(self):
         """
@@ -56,9 +63,13 @@ class Json2XformQuestionValidationTests(TestCase):
         """
         simple_integer_question = {"text": {"f": "fc", "e": "ec"}, "type": "integer", "name": "integer_q", "attributes": {}}
         
+        expected_integer_question_xml = """
+        <input ref="/test/integer_q"><label ref="jr:itext('q_integer_q')"/></input>
+        """
+        
         q = create_question_from_dict(simple_integer_question)
         self.s._add_element(q)
-        self.assertEqual(ctw(q.control()), '?')
+        self.assertEqual(ctw(q.control()), expected_integer_question_xml.strip())
 
 
     def test_simple_date_question_type_multilingual(self):
@@ -66,38 +77,49 @@ class Json2XformQuestionValidationTests(TestCase):
         not sure how date questions should show up.
         """
         simple_date_question = {"text": {"f": "fd", "e": "ed"}, "type": "date", "name": "date_q", "attributes": {}}
-        
+        expected_date_question_xml = """
+        <input ref="/test/date_q"><label ref="jr:itext('q_date_q')"/></input>
+        """
         q = create_question_from_dict(simple_date_question)
         self.s._add_element(q)
-        self.assertEqual(ctw(q.control()), '?')
+        self.assertEqual(ctw(q.control()), expected_date_question_xml.strip())
 
 
     def test_simple_phone_number_question_type_multilingual(self):
         """
         not sure how phone number questions should show up.
         """
-        simple_integer_question = {"text": {"f": "fe", "e": "ee"}, "type": "phone number", "name": "phone_number_q", "attributes": {}}
-
-        q = create_question_from_dict(simple_integer_question)
+        simple_phone_number_question = {"text": {"f": "fe", "e": "ee"}, "type": "phone number", "name": "phone_number_q", "attributes": {}}
+        expected_phone_number_question_xml = """
+        <input ref="/test/phone_number_q"><label ref="jr:itext('q_phone_number_q')"/></input>
+        """
+        
+        q = create_question_from_dict(simple_phone_number_question)
         self.s._add_element(q)
-        self.assertEqual(ctw(q.control()), '?')
+        self.assertEqual(ctw(q.control()), expected_phone_number_question_xml.strip())
 
     def test_simple_select_all_question_multilingual(self):
         """
         not sure how select all questions should show up...
         """
         simple_select_all_question = {"text": {"f": "f choisit", "e": "e choose"}, "attributes": {}, "type": "select all that apply", "name": "select_all_q", "choices": [{"text": {"f": "ff", "e": "ef"}, "value": "f"}, {"text": {"f": "fg", "e": "eg"}, "value": "g"}, {"text": {"f": "fh", "e": "eh"}, "value": "h"}]}
+        expected_select_all_question_xml = """
+        <select ref="/test/select_all_q"><label ref="jr:itext('q_select_all_q')"/></select>
+        """
         
         q = create_question_from_dict(simple_select_all_question)
         self.s._add_element(q)
-        self.assertEqual(ctw(q.control()), '?')
+        self.assertEqual(ctw(q.control()), expected_select_all_question_xml.strip())
 
     def test_simple_decimal_question_multilingual(self):
         """
         not sure how decimal should show up.
         """
         simple_decimal_question = {"text": {"f": "f text", "e": "e text"}, "type": "decimal", "name": "decimal_q", "attributes": {}}
+        expected_decimal_question_xml = """
+        <input ref="/test/decimal_q"><label ref="jr:itext('q_decimal_q')"/></input>
+        """
         
         q = create_question_from_dict(simple_decimal_question)
         self.s._add_element(q)
-        self.assertEqual(ctw(q.control()), '?')
+        self.assertEqual(ctw(q.control()), expected_decimal_question_xml.strip())
