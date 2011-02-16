6e22aacb32cdaf9f69f83f4c184eec47422ccddd
Alex Dorey <dorey415@gmail.com>
2011-02-16 01:37:11 -0500

    these don't test anything

--- json2xform/tests/json2xform_test.py
+++ json2xform/tests/json2xform_test.py
@@ -16,19 +16,4 @@ from ..survey import Survey
 class BasicJson2XFormTests(TestCase):
 
     def test_two_questions_with_same_id_fails(self):
-        """
-        When loading in a form with two identical IDs, the form should
-        not compile. This is a really bad test right now.
-        """
-#        self.assertRaises(Exception, survey_from_json, "json2xform/surveys/super_simple/two_identical_ids.json")
-
-    def test_simple_survey_creation(self):
-        s = Survey(name="test")
-        file_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "..", "surveys", "super_simple", "simple.json"
-            )
-        f = open(file_path)
-        s.load_elements_from_json(f.read())
-        f.close()
-        print s.__unicode__()
+        pass
