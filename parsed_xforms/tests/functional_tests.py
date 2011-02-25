4aa602a73063e588cfb163799ef072eb5d4670e1
Alex Dorey <dorey415@gmail.com>
2011-02-25 10:40:34 -0500

    functional test of map json url.

--- parsed_xforms/tests/functional_tests.py
+++ parsed_xforms/tests/functional_tests.py
@@ -9,10 +9,33 @@ from datetime import datetime
 
 PARSED_XFORMS_URL_ROOT = "/xforms"
 
-class TestFunctional(TestCase):
+import json
 
+from xform_manager.factory import XFormManagerFactory
+xfactory = XFormManagerFactory()
+
+from parsed_xforms.models import xform_instances
+from parsed_xforms import tag
+
+import re
+
+class TestFunctional(TestCase):
+    def setUp(self):
+        survey1 = xfactory.create_simple_instance({
+            'geopoint': '40.765102558006795 -73.97389419555664 300.0 4.0'
+        })
+        survey2 = xfactory.create_simple_instance({
+            'geopoint': '40.80937419746446 -73.95926006317139 300.0 4.0'
+        })
+    
     def test_map_data_is_accessible(self):
         c = Client()
-        
         response = c.get("%s/data/map_data" % PARSED_XFORMS_URL_ROOT)
         self.assertEqual(response.status_code, 200)
+
+        json_response = re.sub("Content-Type: text/html; charset=utf-8", "", str(response)).strip()
+        j = json.loads(json_response)
+        
+        #assert that the factory-created survey shows up in the json
+        #sent to the view
+        self.assertTrue(len(j) > 0)
