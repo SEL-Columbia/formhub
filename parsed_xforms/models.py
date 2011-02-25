aec3e49cd3df2ab2289b48dcf7f481157bca5147
Alex Dorey <dorey415@gmail.com>
2011-02-25 18:39:24 -0500

    we aren't going to be needing this

--- parsed_xforms/models.py
+++ parsed_xforms/models.py
@@ -23,29 +23,6 @@ DISTRICT_ID = u"_district_id"
 ATTACHMENTS = u"_attachments"
 DATE = u"_date"
 
-def create_structure_from_list_of_paths(result_dict, path_list):
-    """
-    Ohh yeahhh. http://www.youtube.com/watch?v=2iE4uEsaBF0
-    """
-    pointer = result_dict
-    key_index = len(path_list)-2
-    for i, p in enumerate(path_list):
-        if p not in pointer:
-            if i < key_index:
-                pointer[p] = {}
-            elif i==key_index:
-                pointer[p] = path_list[key_index+1]
-        if i < key_index:
-            pointer = pointer[p]
-
-def categorize_from_xpath_structure(mongo_dict):
-    result = {}
-    for key in mongo_dict.keys():
-        _vals = key.split('/')
-        _vals.append(mongo_dict[key])
-        create_structure_from_list_of_paths(result, _vals)
-    return result
-
 class ParsedInstance(models.Model):
     # I should rename this model, maybe Survey
     instance = models.OneToOneField(Instance, related_name="parsed_instance")
@@ -55,7 +32,7 @@ class ParsedInstance(models.Model):
 
     def _set_phone(self, doc):
         self.phone, created = Phone.objects.get_or_create(imei=doc[tag.IMEI])
-
+        
         # when a phone update comes in update the information about the phone
         if doc[tag.INSTANCE_DOC_NAME]==PHONE_UPDATE:
             d = {
@@ -130,11 +107,9 @@ class ParsedInstance(models.Model):
             "This instance hasn't been parsed into Mongo"
             )
     
-    def json_data(self):
-        return categorize_from_xpath_structure(self.get_from_mongo())
-    
     def save(self, *args, **kwargs):
         doc = self.instance.get_dict()
+        
         self._set_phone(doc)
         self._set_surveyor(doc)
         self._set_district(doc)
