85cbb3e0e3f3705dcb69ffb8b1338fb03902f460
Alex Dorey <dorey415@gmail.com>
2011-03-21 19:27:16 -0400

    updating "reparse" test.

--- parsed_xforms/tests/reparse_everything.py
+++ parsed_xforms/tests/reparse_everything.py
@@ -8,14 +8,24 @@ from xform_manager.factory import XFormManagerFactory
 
 from parsed_xforms.models import *
 from surveyor_manager.models import *
+from xform_manager.models import *
 
 class TestReparseEverything(TestCase):
     def setUp(self):
-        print "HERE"
         self.xf = XFormManagerFactory()
-        self.xf.create_registration_instance({'name':'John', 'device_id': '90909'})
-        self.xf.create_simple_instance({'device_id':'90909', 'geopoint': '10.100 10.100 300.0 4.0'})
+        self.x = self.xf.create_registration_instance({'name':'John', 'device_id': '90909'})
+        self.i = self.xf.create_simple_instance({'device_id':'90909', 'geopoint': '10.100 10.100 300.0 4.0'})
     
-    def tes_basic_reparse_method_works(self):
-#        self.assertEqual(ParsedInstance.objects.count(), 1)
+    def test_basic_reparse_method_works(self):
+        self.assertEqual(ParsedInstance.objects.count(), 2)
+        self.assertEqual(Surveyor.objects.count(), 1)
+        
+        #Instance object has a method "reparse"
+        for i in Instance.objects.all():
+            #at the moment, reparse just deletes the parsed_instance
+            #and then resaves, forcing a reparse instance.
+            i.reparse()
+        
+        #THESE TESTS FAIL (and will continue to do so until we fix the code)
+#        self.assertEqual(ParsedInstance.objects.count(), 2)
 #        self.assertEqual(Surveyor.objects.count(), 1)
