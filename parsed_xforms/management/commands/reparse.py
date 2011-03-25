294d4504b9f0a1ef5d034628961f3418695813bd
Alex Dorey <dorey415@gmail.com>
2011-03-25 15:43:50 -0400

    changed management command for reparse.

--- parsed_xforms/management/commands/reparse.py
+++ parsed_xforms/management/commands/reparse.py
@@ -2,13 +2,10 @@
 # vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 
 from django.core.management.base import BaseCommand
-from ...models import xform_instances, Instance
+from parsed_xforms.reparse import *
 
 class Command(BaseCommand):
     help = "Delete and recreate parsed instances."
 
     def handle(self, *args, **kwargs):
-        xform_instances.remove()
-        for i in Instance.objects.all():
-            print "*"
-            i.save()
+        reparse_all(debug=True)
