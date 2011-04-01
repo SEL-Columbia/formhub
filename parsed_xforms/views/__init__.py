85d794f2ca8e0ee3ce60a9d48069794c46ee09e0
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-01 11:32:31 -0400

    Added a view to show pretty survey questions and responses, and a list of urls to the images submitted with a survey. Both of these views return json.

--- parsed_xforms/views/__init__.py
+++ parsed_xforms/views/__init__.py
@@ -1,3 +1,4 @@
 from old_views import *
 from xls_export import xls
 from map_json import map_data_points, links_to_json_for_lga_maps
+from single_survey_submission import survey_responses, survey_media_files
