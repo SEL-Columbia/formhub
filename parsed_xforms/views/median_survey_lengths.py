695529a315d4a4142a530acbdbea92b9cbefbc3f
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-05 10:59:20 -0400

    Minor edit.

--- parsed_xforms/views/median_survey_lengths.py
+++ parsed_xforms/views/median_survey_lengths.py
@@ -8,7 +8,7 @@ def median_survey_lengths(request):
     Get the median time spent on each survey.
     """
     times = defaultdict(list)
-    for pi in ParsedInstance.objects.all().iterator():
+    for pi in ParsedInstance.objects.all():
         if pi.instance.xform:
             survey_title = pi.instance.xform.title
             if pi.end_time is not None and pi.start_time is not None:
@@ -17,6 +17,7 @@ def median_survey_lengths(request):
         times[key].sort()
     result = {}
     for k in times.keys():
-        result[k] = (times[k][len(times[k])/2], len(times[k]))
+        title_with_count = unicode(len(times[k])) + u" " + k
+        result[title_with_count] = times[k][len(times[k])/2]
     return render_to_response("dict.html", {"dict":result})
     
