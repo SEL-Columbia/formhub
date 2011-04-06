caa8f3239d3d6d152193c4a3171226e7538b293c
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-06 18:37:40 -0400

    Put permission required on gps view for map, and got rid of link list.

--- parsed_xforms/views/__init__.py
+++ parsed_xforms/views/__init__.py
@@ -1,6 +1,6 @@
 from old_views import *
 from xls_export import xls
-from map_json import map_data_points, links_to_json_for_lga_maps
+from map_json import map_data_points
 from single_survey_submission import survey_responses, survey_media_files
 from median_survey_lengths import median_survey_lengths
 from redirect_if_unauthorized import access_denied
--- parsed_xforms/views/map_json.py
+++ parsed_xforms/views/map_json.py
@@ -2,9 +2,9 @@ from common_tags import LGA_ID, START_TIME, SURVEYOR_NAME, \
     INSTANCE_DOC_NAME, ATTACHMENTS, GPS
 from parsed_xforms.models import xform_instances
 from utils import json_response
+from deny_if_unauthorized import deny_if_unauthorized
 
-from django.http import HttpResponse #, HttpResponseBadRequest, HttpResponseRedirect
-
+@deny_if_unauthorized()
 def map_data_points(request, lga_id):
     """
     The map list needs these attributes for each survey to display
@@ -25,16 +25,3 @@ def map_data_points(request, lga_id):
     mongo_query = xform_instances.find(spec=match_lga, fields=fields)
     list_of_dicts = list(mongo_query)
     return json_response(list_of_dicts)
-
-from django.core.urlresolvers import reverse
-from nga_districts.models import LGA
-from parsed_xforms.models import ParsedInstance
-
-def links_to_json_for_lga_maps(request):
-    result = u""
-    for lga in LGA.get_ordered_phase2_query_set():
-        d = {u"count" : ParsedInstance.objects.filter(lga=lga).count(),
-             u"url" : reverse(map_data_points, kwargs={'lga_id' : lga.id}),
-             u"name" : lga.name,}
-        result += u'%(count)s: <a href="%(url)s">%(name)s</a> <br/>' % d
-    return HttpResponse(result)
