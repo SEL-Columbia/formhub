bc6ed4a95c18fb03c90f1c591865191cb564d9fa
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-06 17:57:21 -0400

    Cleaned up decorator to have three cases, it now handles users that have permission, users that haven't logged in yet, and users that don't have permission. This is what Django's permission_required decorator should be.

--- parsed_xforms/views/deny_if_unauthorized.py
+++ parsed_xforms/views/deny_if_unauthorized.py
@@ -1,25 +1,36 @@
-from functools import update_wrapper, wraps
-from django.utils.decorators import available_attrs
-from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def access_denied(request):
     context = RequestContext(request)
     return render_to_response(
-        "homepage.html",
+        "access_denied.html",
         context_instance=context
         )
+
+from functools import update_wrapper, wraps
+from django.utils.decorators import available_attrs
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.utils.http import urlquote
 
 def deny_if_unauthorized(permission="xform.can_view"):
     """
     If the user requesting this page has 'permission' show them the
-    view, otherwise show them our access denied page.
+    view, if the user is anonymous redirect them to the login page,
+    otherwise show them our access denied page.
     """
     def decorator(view_func):
         def _wrapped_view(request, *args, **kwargs):
             user = request.user
             if user.has_perm(permission):
                 return view_func(request, *args, **kwargs)
-            return access_denied(request)
+            elif user.is_anonymous():
+                path = urlquote(request.get_full_path())
+                pair = (settings.LOGIN_URL, path)
+                redirect_url = '%s?next=%s' % pair
+                return HttpResponseRedirect(redirect_url)
+            else:
+                return access_denied(request)
         return wraps(view_func, assigned=available_attrs(view_func))(_wrapped_view)
     return decorator
