9d09ce07c231b1d176c51eecb95e7d4bfc9a3d8a
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-11 13:32:37 -0400

    Fixed import.

--- parsed_xforms/views/__init__.py
+++ parsed_xforms/views/__init__.py
@@ -3,4 +3,4 @@ from xls_export import xls
 from map_json import map_data_points
 from single_survey_submission import survey_responses, survey_media_files
 from median_survey_lengths import median_survey_lengths
-from redirect_if_unauthorized import access_denied
+from deny_if_unauthorized import access_denied
