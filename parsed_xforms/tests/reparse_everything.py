047151f2bf89c371d800db634415b54ac83b6483
Andrew Marder <andrew.ei.marder@gmail.com>
2011-05-03 03:45:25 -0400

    Switched over to using unicode strings.

--- parsed_xforms/tests/reparse_everything.py
+++ parsed_xforms/tests/reparse_everything.py
@@ -16,8 +16,8 @@ xform_instances = settings.MONGO_DB.instances
 class TestReparseEverything(TestCase):
     def setUp(self):
         self.xf = XFormManagerFactory()
-        self.x = self.xf.create_registration_instance({'name':'John', 'device_id': '90909'})
-        self.i = self.xf.create_simple_instance({'device_id':'90909', 'geopoint': '10.100 10.100 300.0 4.0'})
+        self.x = self.xf.create_registration_instance({u'name':u'John', u'device_id': u'90909'})
+        self.i = self.xf.create_simple_instance({u'device_id':u'90909', u'geopoint': u'10.100 10.100 300.0 4.0'})
     
     def test_basic_reparse_method_works(self):
         self.assertEqual(ParsedInstance.objects.count(), 2)
