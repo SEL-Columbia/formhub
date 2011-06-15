8097c31083d6f23e392465ec08cfc621c69782ae
Alex Dorey <dorey415@gmail.com>
2011-06-14 23:03:12 -0400

    removing some stuff (custom_settings, fabfile, factory) we'll want to replace later probably.

--- factory.py
+++
@@ -1,7 +0,0 @@
-#factories can be git-downloaded and installed from here:
-#https://github.com/mirhampt/django-factories
-
-from surveyor_manager.factory import SurveyorManagerFactory
-from phone_manager.factory import PhoneManagerFactory
-from locations.factory import DistrictFactory
-from xform_manager.factory import XFormManagerFactory
