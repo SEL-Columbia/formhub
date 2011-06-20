b4aab64326c000f0704010972288b8b78fa408af
Alex Dorey <dorey415@gmail.com>
2011-06-20 14:17:42 -0400

    using "load_fixtures" management command to load lgas and xform_manager_dataset

---
+++ nga_districts/management/commands/load_fixtures.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+import os
+
+class Command(BaseCommand):
+    help = "Load the LGAs from fixtures."
+
+    def handle(self, *args, **kwargs):
+        call_command('syncdb', interactive=False)
+        
+        if not os.path.exists('xform_manager_dataset.json'):
+            raise Exception("Download and unpack xfmgr.json into the project dir.")
+        
+        for file_name in ['zone.json', 'state.json', 'lga.json']:
+            path = os.path.join(['nga_districts', 'fixtures', file_name])
+            call_command('loaddata', file_name)
+        
+        call_command('loaddata', 'xform_manager_dataset.json')
