7a0ac07a38ae4edc5f7ca8d21090178b5471208a
Alex Dorey <dorey415@gmail.com>
2011-06-20 16:09:43 -0400

    fixing error message to whine about the right file.

--- nga_districts/management/commands/load_fixtures.py
+++ nga_districts/management/commands/load_fixtures.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
         call_command('syncdb', interactive=False)
         
         if not os.path.exists('xform_manager_dataset.json'):
-            raise Exception("Download and unpack xfmgr.json into the project dir.")
+            raise Exception("Download and unpack xform_manager_dataset.json into the project dir.")
         
         for file_name in ['zone.json', 'state.json', 'lga.json']:
             path = os.path.join(['nga_districts', 'fixtures', file_name])
