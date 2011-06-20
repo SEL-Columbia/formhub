d0f45bb734711e88413c6aacc3e25dbcd2dd8ca9
Alex Dorey <dorey415@gmail.com>
2011-06-20 14:15:43 -0400

    bson is a mongo thing?

--- parsed_xforms/views/utils.py
+++ parsed_xforms/views/utils.py
@@ -1,7 +1,8 @@
 import json
-from bson import json_util
+#from bson import json_util
 from django.http import HttpResponse
 
 def json_response(pyobj):
-    json_str = json.dumps(pyobj, default=json_util.default)
+#    json_str = json.dumps(pyobj, default=json_util.default)
+    json_str = json.dumps(pyobj)
     return HttpResponse(json_str)
