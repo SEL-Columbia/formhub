98d36d329a844689a1736c17e5673364d5ca8679
Alex Dorey <dorey415@gmail.com>
2011-06-20 20:10:15 -0400

    These models are needed by the views. I think we should phase these out slowly.

--- parsed_xforms/models/__init__.py
+++ parsed_xforms/models/__init__.py
@@ -1,12 +1,5 @@
-"""
-
-I'm commenting out these models so we can bring them in as needed.
-
-I'm doing this because I don't want to comment out the whole app
- --which contains templates and static content. [AD]
-"""
-#from parsed_instance import ParsedInstance
-#from registration import Registration
-#from data_dictionary import DataDictionary
-#from instance_modification import InstanceModification
-#import common_tags
+from parsed_instance import ParsedInstance
+from registration import Registration
+from data_dictionary import DataDictionary
+from instance_modification import InstanceModification
+import common_tags
