4596771cdcbf41da1ecfc4f13669fa05b7445d10
Andrew Marder <andrew.ei.marder@gmail.com>
2011-06-21 11:03:03 -0700

    Started loading key renames from a csv file.

--- nga_districts/management/commands/load_fixtures.py
+++ nga_districts/management/commands/load_fixtures.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 import os
-from facilities.models import Variable
+from facilities.models import Variable, KeyRename
+from utils.csv_reader import CsvReader
 
 
 class Command(BaseCommand):
@@ -10,7 +11,12 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         call_command('syncdb', interactive=False)
         self.load_lgas()
-        self.create_variables()
+        csvs = [
+            (Variable, os.path.join('facilities', 'fixtures', 'variables.csv')),
+            (KeyRename, os.path.join('facilities', 'fixtures', 'key_renames.csv')),
+            ]
+        for model, path in csvs:
+            self.create_objects_from_csv(model, path)
         self.load_surveys()
         self.create_admin_user()
 
@@ -18,12 +24,10 @@ class Command(BaseCommand):
         for file_name in ['zone.json', 'state.json', 'lga.json']:
             call_command('loaddata', file_name)
 
-    def create_variables(self):
-        from utils.csv_reader import CsvReader
-        path = os.path.join('facilities', 'fixtures', 'variables.csv')
+    def create_objects_from_csv(self, model, path):
         csv_reader = CsvReader(path)
         for d in csv_reader.iter_dicts():
-            Variable.objects.get_or_create(**d)
+            model.objects.get_or_create(**d)
 
     def load_surveys(self):
         if not os.path.exists('xform_manager_dataset.json'):
