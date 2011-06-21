806346fab927ca746f9e2d7506768a7f32179992
Alex Dorey <dorey415@gmail.com>
2011-06-21 10:06:20 -0400

    using httpresponse to expose JSON.

--- facilities/views.py
+++ facilities/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 import json
@@ -13,8 +14,9 @@ def home(request):
 def facilities_for_site(request, site_id):
     context = RequestContext(request)
     lga = LGA.objects.get(slug=site_id)
-    context.site = lga
-    context.site_name = lga.name
-    context.ftypes = list(FacilityType.objects.all())
-    context.facility_data = json.dumps(Facility.get_latest_data_by_lga(lga))
-    return render_to_response("facilities_by_lga.html", context_instance=context)
+#    context.site = lga
+#    context.site_name = lga.name
+#    context.ftypes = list(FacilityType.objects.all())
+#    context.facility_data = json.dumps(Facility.get_latest_data_by_lga(lga))
+    return HttpResponse(json.dumps(Facility.get_latest_data_by_lga(lga)))
+#    return render_to_response("facilities_by_lga.html", context_instance=context)
