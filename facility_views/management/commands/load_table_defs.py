d8a8eba175a52af0a21d8775045731a4cb6384bb
Alex Dorey <dorey415@gmail.com>
2011-06-29 11:05:08 -0400

    changed around how table defs are added & stored. set up a custom DOM event to bind to.

--- facility_views/management/commands/load_table_defs.py
+++ facility_views/management/commands/load_table_defs.py
@@ -23,10 +23,25 @@ class Command(BaseCommand):
         FacilityTable.objects.all().delete()
         TableColumn.objects.all().delete()
         ColumnCategory.objects.all().delete()
+        
+        subgroups = {}
+        sgs = list(CsvReader(os.path.join("facility_views","table_defs", "subgroups.csv")).iter_dicts())
+        for sg in sgs:
+            subgroups[sg['slug']] = sg['name']
+
         for name, slug in table_types:
             curtable = FacilityTable.objects.create(name=name, slug=slug)
             csv_reader = CsvReader(os.path.join("facility_views","table_defs", "%s.csv" % slug))
             for input_d in csv_reader.iter_dicts():
+                subs = []
+                for sg in input_d['subgroups'].split(" "):
+                    if sg in subgroups:
+                        subs.append({
+                            'name': subgroups[sg],
+                            'slug': sg
+                        })
+                for sub in subs:
+                    curtable.add_column(sub)
                 d = {
                     'name': input_d['name'],
                     'slug': input_d['slug'],
