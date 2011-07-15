ac7fb32f1532961b4a6bb1ec39e3ea7359cb772a
Andrew Marder <andrew.ei.marder@gmail.com>
2011-07-15 14:05:10 -0700

    Removed apps for phones and surveyors.

--- surveyor_manager/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Surveyor(User):
-    name = models.CharField(max_length=100)
-
-    def __unicode__(self):
-        return "Surveyor: %s" % self.name
