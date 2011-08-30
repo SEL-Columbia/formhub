40432de36988f881f9971e4540bf4bd2177c1aef
Andrew Marder <andrew.ei.marder@gmail.com>
2011-08-30 12:10:56 -0700

    Got rid of a bunch of Nigeria specific apps.

--- main/views.py
+++ main/views.py
@@ -1,12 +1,8 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 from django.http import HttpResponseRedirect
-from django.template import RequestContext
-from django.shortcuts import render_to_response
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import Permission, Group
-from django.db.models import Count
-from nga_districts.models import LGA
 from xls2xform.views import home, get_survey
 from xform_manager.models import XForm
 
@@ -44,16 +40,3 @@ def publish(request, survey_id):
     xform, created = XForm.objects.get_or_create(**kwargs)
     xform.groups.add(default_group(request.user))
     return HttpResponseRedirect("/")
-
-
-def list_active_lgas(request):
-    context = RequestContext(request)
-    context.site_title = "NMIS: LGA List"
-    context.lgas = LGA.objects.annotate(facility_count=Count('facilities')).filter(facility_count__gt=0)
-    return render_to_response("list_active_lgas.html", context_instance=context)
-
-def site_description(request):
-    return render_to_response("site_description.html")
-
-def baseline_redirect(request):
-    return HttpResponseRedirect("/baseline/")
--- urls.py
+++ urls.py
@@ -9,28 +9,17 @@ OPT_GROUP_REGEX = "((?P<group_name>[^/]+)/)?"
 
 from main import views as main_views
 
-from uis_r_us import views as ui
 from survey_photos.views import photo_redirect
 
 urlpatterns = patterns('',
-    url(r'^~(?P<reqpath>\S*)', ui.dashboard),
     url(r'^$', main_views.index),
 
+    url(r'^xform_manager/', include('nmis.xform_manager.urls')),
     url(r"^%sformList$" % OPT_GROUP_REGEX, xform_manager_views.formList),
     url(r"^%ssubmission$" % OPT_GROUP_REGEX, xform_manager_views.submission),
-    url(r'^xform_manager/', include('nmis.xform_manager.urls')),
     url(r'^xls2xform/', include('nmis.xls2xform.urls')),
     url(r'^main/', include('nmis.main.urls')),
 
-    url(r'^description/', main_views.site_description),
-    url(r'^facilities/', include('facilities.urls')),
-    url(r'^facility_variables', ui.variable_data),
-    url(r'^lgas/', include('nmis.nga_districts.urls')),
-    url(r'^lgas/$', main_views.list_active_lgas),
-    url(r'^modes/(?P<mode_data>\S*)$', ui.modes),
-    url(r'^mustache/(?P<template_name>\w+)$', ui.mustache_template),
-    url(r'^test_map/$', ui.test_map),
-    url(r'^resources/', include('resources.urls')),
     url(r'^user_management/', include('user_management.urls')),
     url(r'^survey_photos/(?P<size>\S+)/(?P<photo_id>\S+)$', photo_redirect),
 
