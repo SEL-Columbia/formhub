227ddd219f8a866cbc1d143172893e5077297c09
Andrew Marder <andrew.ei.marder@gmail.com>
2011-08-30 10:47:42 -0700

    Removed a bunch of apps that were nmis specific.

--- facilities/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from old_tests import BasicDataTest, CalculatedVariableTest, \
-                GapAnalysisTest, LGAIndicatorTest, PassDataToPage, \
-                ScoreVariableTest
-from import_tests import ImportDataTest
--- resources/views.py
+++
@@ -1,9 +0,0 @@
-# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-
-from django.http import HttpResponseRedirect
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-
-def resources(request):
-    context = RequestContext(request)
-    return render_to_response("resources.html", context_instance=context)
