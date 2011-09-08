e28bb3cca7a1f8d23bc43040e72e74ad54150209
Andrew Marder <andrew.ei.marder@gmail.com>
2011-09-08 13:08:28 -0700

    Cleaned up a lot.

--- main/views.py
+++ main/views.py
@@ -3,17 +3,28 @@
 from django.http import HttpResponseRedirect
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import Permission, Group
-from xls2xform.views import home, get_survey
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 from xform_manager.models import XForm
 
 from xls2xform.exporter import export_survey
+from xls2xform.views import QuickConverter
+
 
 def index(request):
-    kwargs = {
-        'extra_links': 'publish_link.html',
-        'extra_section': 'published_xforms.html',
-        }
-    return home(request, **kwargs)
+    if request.method == 'POST':
+        form = QuickConverter(request.POST, request.FILES)
+        if form.is_valid():
+            survey = form.get_survey()
+            publish(request.user, survey)
+            form = QuickConverter()
+    else:
+        form = QuickConverter()
+
+    context = RequestContext(request)
+    context.form = form
+    return render_to_response("dashboard.html", context_instance=context)
 
 
 def default_group(user):
@@ -32,12 +43,12 @@ def default_group(user):
     return group
 
 
-def publish(request, survey_id):
-    survey = export_survey(get_survey(request.user, survey_id))
+def publish(user, survey):
     kwargs = {
         'xml': survey.to_xml(),
         'downloadable': True,
+        'user': user,
         }
     xform, created = XForm.objects.get_or_create(**kwargs)
-    xform.groups.add(default_group(request.user))
-    return HttpResponseRedirect("/")
+
+    # need to also make a data dictionary
