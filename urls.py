e0c3dbb5e414e533014d2883dc9b017be97937ed
Andrew Marder <andrew.ei.marder@gmail.com>
2011-09-12 14:52:41 -0700

    Got some more tests passing.

--- urls.py
+++ urls.py
@@ -11,7 +11,6 @@ from survey_photos.views import photo_redirect
 urlpatterns = patterns('',
     url(r'^$', main_views.dashboard),
 
-    url(r'^xform_manager/', include('nmis.xform_manager.urls')),
     url(r'^xls2xform/', include('nmis.xls2xform.urls')),
     url(r'^parsed_xforms/', include('nmis.parsed_xforms.urls')),
 
@@ -20,4 +19,5 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^', include('nmis.xform_manager.urls')),
 )
--- xform_manager/tests/instance_creation_test.py
+++ xform_manager/tests/instance_creation_test.py
@@ -1,9 +1,12 @@
-from django.test import TestCase, Client
+import glob
+import os
+from django.test import TestCase
+from django.contrib.auth.models import User
 from xform_manager.models import XForm, Instance
-import os, glob
+
 
 def open_all_files(path):
-    file_paths = glob.glob( os.path.join(path, "*") )
+    file_paths = glob.glob(os.path.join(path, "*"))
     result = {}
     for file_path in file_paths:
         if file_path.endswith(".jpg"):
@@ -13,12 +16,13 @@ def open_all_files(path):
             result[file_path] = open(file_path)
     return result
 
+
 def create_post_data(path):
-    xml_files = glob.glob( os.path.join(path, "*.xml") )
-    if len(xml_files)!=1:
+    xml_files = glob.glob(os.path.join(path, "*.xml"))
+    if len(xml_files) != 1:
         raise Exception("There should be a single XML file in this directory.")
     xml_file = open(xml_files[0])
-    post_data = {"xml_submission_file" : xml_file}
+    post_data = {"xml_submission_file": xml_file}
 
     for jpg in glob.glob(os.path.join(path, "*.jpg")):
         # note the "rb" mode is to open a binary file
@@ -26,6 +30,7 @@ def create_post_data(path):
         post_data[jpg] = image_file
 
     return post_data
+
 
 def get_absolute_path(subdirectory):
     return os.path.join(
@@ -37,6 +42,7 @@ def get_absolute_path(subdirectory):
 class TestWaterSubmission(TestCase):
 
     def setUp(self):
+        self.user = User.objects.create(username="bob")
         absolute_path = get_absolute_path("forms")
         open_forms = open_all_files(absolute_path)
         for path, open_file in open_forms.items():
@@ -50,9 +56,7 @@ class TestWaterSubmission(TestCase):
                 ))
         xml = f.read()
         f.close()
-        xform = XForm.objects.create(
-            xml=xml
-            )
+        XForm.objects.create(xml=xml)
 
     def test_form_submission(self):
         f = open(os.path.join(
@@ -61,9 +65,7 @@ class TestWaterSubmission(TestCase):
                 ))
         xml = f.read()
         f.close()
-        instance = Instance.objects.create(
-            xml=xml
-            )
+        Instance.objects.create(xml=xml)
 
     def test_instance_creation(self):
         xml_file = open(os.path.join(
@@ -81,9 +83,9 @@ class TestWaterSubmission(TestCase):
 
         # ODK Collect uses the name of the jpg file as the key in the
         # post.
-        postdata = {"xml_submission_file" : xml_file,
-                    "1300221157303.jpg" : image_file}
-        response = self.client.post('/submission', postdata)
+        postdata = {"xml_submission_file": xml_file,
+                    "1300221157303.jpg": image_file}
+        response = self.client.post('/bob/submission', postdata)
         self.failUnlessEqual(response.status_code, 201)
 
     def test_data_submission(self):
@@ -91,5 +93,5 @@ class TestWaterSubmission(TestCase):
         for subdirectory in subdirectories:
             path = get_absolute_path(subdirectory)
             postdata = create_post_data(path)
-            response = self.client.post('/submission', postdata)
+            response = self.client.post('/bob/submission', postdata)
             self.failUnlessEqual(response.status_code, 201)
