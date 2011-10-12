430d819c97879202c18de91fc2fd0f07b125ff8b
Andrew Marder <andrew.ei.marder@gmail.com>
2011-10-11 20:57:10 -0400

    Added ParsedInstance to the admin so we can look at gps coordinates easily.

--- odk_viewer/admin.py
+++ odk_viewer/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models.data_dictionary import DataDictionary
+from models.parsed_instance import ParsedInstance
 
 admin.site.register(DataDictionary)
+admin.site.register(ParsedInstance)
