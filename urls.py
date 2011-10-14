e8874c8db7701fcfa7d0734d279af5c907a94f89
Andrew Marder <andrew.ei.marder@gmail.com>
2011-10-14 17:36:04 -0400

    Added static serve view for media files in development.

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,4 +13,7 @@ urlpatterns = patterns('',
     url(r'^', include('main.urls')),
     url(r'^odk_viewer/', include('odk_viewer.urls')),
     url(r'^(?P<username>[^/]+)/', include('odk_logger.urls')),
+
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
 )
