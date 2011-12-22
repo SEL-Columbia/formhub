9603c30d3934d6a9590ddeb6d2fe61b262a8997e
Mark Johnston <mejymejy@gmail.com>
2011-12-22 16:35:19 -0500

    remove debug statement

--- main/gravatar.py
+++ main/gravatar.py
@@ -7,5 +7,4 @@ def get_gravatar_img_link(user):
     size = 40
     gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
     gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
-    print "gravatar url for %s: %s" % (email, gravatar_url)
     return gravatar_url
