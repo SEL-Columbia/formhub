046ea91612ae4fc0d96e6879ff22c495df9babd0
pld <peter@helioid.com>
2011-12-23 21:24:09 -0500

    better method names

--- main/tests/test_form_show.py
+++ main/tests/test_form_show.py
@@ -12,7 +12,7 @@ class TestFormShow(MainTestCase):
         MainTestCase.setUp(self)
         self._create_user_and_login()
         xls_path = os.path.join(self.this_directory, "fixtures", "transportation", "transportation.xls")
-        self._publish_xls_file(xls_path)
+        response = self._publish_xls_file(xls_path)
         self.assertEqual(XForm.objects.count(), 1)
         self.xform = XForm.objects.all()[0]
         self.url = reverse(show, kwargs={'username': self.user.username, 'id_string': self.xform.id_string})
@@ -22,11 +22,11 @@ class TestFormShow(MainTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.xform.id_string)
 
-    def test_hide_from_not_user(self):
+    def test_hide_from_anon(self):
         response = self.anon.get(self.url)
         self.assertEqual(response.status_code, 302)
 
-    def test_show_to_not_user_if_public(self):
+    def test_show_to_anon_if_public(self):
         self.xform.shared = True
         self.xform.save()
         response = self.anon.get(self.url)
