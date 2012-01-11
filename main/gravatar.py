6069b7a55a879f5433761c7bf7f70d3519cdafd7
pld <peter@helioid.com>
2012-01-11 12:50:16 -0500

    default avatar

--- main/gravatar.py
+++ main/gravatar.py
@@ -1,9 +1,9 @@
 import urllib, hashlib
-
+from django.core.urlresolvers import reverse
 
 def get_gravatar_img_link(user):
     email = user.email
-    default = "http://www.example.com/default.jpg"
+    default = "http://www.formhub.org/static/images/formhub_avatar.png"
     size = 40
     gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
     gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
