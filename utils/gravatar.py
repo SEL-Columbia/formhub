b5b13126372e7aed6d0d28a582e076c82db29668
pld <peter@helioid.com>
2012-01-18 09:28:41 -0500

    gravatar ssl

--- utils/gravatar.py
+++ utils/gravatar.py
@@ -3,8 +3,8 @@ from django.core.urlresolvers import reverse
 
 def get_gravatar_img_link(user):
     email = user.email
-    default = "http://www.formhub.org/static/images/formhub_avatar.png"
+    default = "https://formhub.org/static/images/formhub_avatar.png"
     size = 40
-    gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+    gravatar_url = "https://secure.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
     gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
     return gravatar_url
