13aa6f839a8db8998fb0eff4d8210508a958cf9e
Mark Johnston <mejymejy@gmail.com>
2012-01-31 13:31:28 -0500

    updates to facilitate moving to amazon-s3

---
+++ odk_logger/management/commands/move_media_to_s3.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+
+from django.core.management.base import BaseCommand
+from django.core.files.storage import get_storage_class
+from odk_logger.models.attachment import Attachment
+from odk_logger.models.attachment import upload_to as attachment_upload_to
+from odk_logger.models.xform import XForm
+from odk_logger.models.xform import upload_to as xform_upload_to
+import sys
+
+
+class Command(BaseCommand):
+    help = "Moves all attachments and xls files to s3 from the local file system storage."
+
+    def handle(self, *args, **kwargs):
+        try:
+            fs = get_storage_class('django.core.files.storage.FileSystemStorage')()
+            s3 = get_storage_class('storages.backends.s3boto.S3BotoStorage')()
+        except:
+            print "Missing necessary libraries. Try running: pip install -r requirements-s3.pip"
+            sys.exit(1)
+
+        default_storage = get_storage_class()()
+        if default_storage.__class__ != s3.__class__:
+            print "You must first set your default storage to s3 in your local_settings.py file."
+            sys.exit(1)
+
+        classes_to_move = [
+            (Attachment, 'media_file', attachment_upload_to),
+            (XForm, 'xls', xform_upload_to),
+        ]
+
+        for cls, file_field, upload_to in classes_to_move:
+            print "Moving %ss to s3..." % cls.__name__
+            for i in cls.objects.all():
+                f = getattr(i, file_field)
+                old_filename = f.name
+                if f.name and fs.exists(f.name) and not s3.exists(upload_to(i, f.name)):
+                    f.save(fs.path(f.name), fs.open(fs.path(f.name)))
+                    print "\t+ '%s'\n\t---> '%s'" % (fs.path(old_filename), f.url)
+                else:
+                    print "\t- (f.name=%s, fs.exists(f.name)=%s, not s3.exists(upload_to(i, f.name))=%s)" \
+                        % (f.name, fs.exists(f.name), not s3.exists(upload_to(i, f.name)))
