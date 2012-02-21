33870a0952478c72d955bebed33b21e9b653ce6b
pld <peter@helioid.com>
2012-02-21 12:02:08 -0500

    pull exists w/separate funciton

--- main/models/user_profile.py
+++ main/models/user_profile.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.db import models
 from utils.country_field import COUNTRIES
-from utils.gravatar import get_gravatar_img_link
+from utils.gravatar import get_gravatar_img_link, gravatar_exists
 
 class UserProfile(models.Model):
     # This field is required.
@@ -18,26 +18,14 @@ class UserProfile(models.Model):
     description = models.CharField(max_length=255, blank=True)
     require_auth = models.BooleanField(default=False,
             verbose_name="Require Phone Authentication")
-    gravatar_data = None
 
     @property
     def gravatar(self):
-        if not self.gravatar_data:
-            self.set_gravatar_data()
-        return self.gravatar_data['gravatar']
+        return get_gravatar_img_link(self.user)
 
     @property
     def gravatar_exists(self):
-        if not self.gravatar_data:
-            self.set_gravatar_data()
-        return self.gravatar_data['exists']
-
-    def set_gravatar_data(self):
-        data = get_gravatar_img_link(self.user)
-        self.gravatar_data = {
-            'gravatar': data[0],
-            'exists': data[1]
-        }
+        return gravatar_exists(self.user)
 
     class Meta:
         app_label = 'main'
