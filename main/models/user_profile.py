041b021f6ca7272ac90639b94029717c61819867
pld <peter@helioid.com>
2012-02-21 18:55:09 -0500

    remove unused signal import

--- main/models/user_profile.py
+++ main/models/user_profile.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 from django.db import models
 from utils.country_field import COUNTRIES
 from utils.gravatar import get_gravatar_img_link
