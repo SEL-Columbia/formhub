ba1d3190672c97a7ebd440a10e423dd6232d5826
pld <peter@helioid.com>
2012-02-23 17:35:45 -0500

    view by link store

--- main/models/meta_data.py
+++ main/models/meta_data.py
@@ -38,6 +38,11 @@ class MetaData(models.Model):
     data_file_type = models.CharField(max_length=255, null=True)
 
     @staticmethod
+    def public_link(xform, data_value=False):
+        data_type = 'public_link'
+        return unique_type_for_form(xform, data_type, data_value)
+
+    @staticmethod
     def form_license(xform, data_value=None):
         data_type = 'form_license'
         return unique_type_for_form(xform, data_type, data_value)
