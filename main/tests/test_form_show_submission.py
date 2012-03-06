35932bce4c1d7fd933af85f82ec409ca33c86c76
pld <peter@helioid.com>
2012-03-06 15:07:53 -0500

    finish show form submission test closes #185

--- main/tests/test_form_show_submission.py
+++ main/tests/test_form_show_submission.py
@@ -1,6 +1,8 @@
 from test_base import MainTestCase
-from main.views import show_submission
+from main.views import home, show_submission
+from odk_viewer.views import survey_responses
 from django.core.urlresolvers import reverse
+from guardian.shortcuts import assign
 
 class TestFormShowSubmission(MainTestCase):
 
@@ -8,13 +10,34 @@ class TestFormShowSubmission(MainTestCase):
         MainTestCase.setUp(self)
         self._create_user_and_login()
         self._publish_transporation_form_and_submit_instance()
-        self.instance = self.xform.surveys.reverse()[0]
+        self.submission= self.xform.surveys.reverse()[0]
         self.url = reverse(show_submission, kwargs={
             'username': self.user.username,
             'id_string': self.xform.id_string,
-            'uuid': self.instance.uuid,
+            'uuid': self.submission.uuid,
         })
+        self.survey_url = reverse(survey_responses, kwargs={
+                'pk': self.submission.parsed_instance.pk })
 
     def test_get_form_by_uuid(self):
         response = self.client.get(self.url)
         self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.base_url + self.survey_url, response['Location'])
+
+    def test_no_anon_get_form_by_uuid(self):
+        response = self.anon.get(self.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.base_url + reverse(home), response['Location'])
+
+    def test_no_without_perm_get_form_by_uuid(self):
+        self._create_user_and_login('alice', 'alice')
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.base_url + reverse(home), response['Location'])
+
+    def test_with_perm_get_form_by_uuid(self):
+        self._create_user_and_login('alice', 'alice')
+        assign('view_xform', self.user, self.xform)
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.base_url + self.survey_url, response['Location'])
