860af17082321f50b4caaebfd810f530638d6ba5
pld <peter@helioid.com>
2012-03-12 16:45:59 -0400

    remove unused code; separate addPoints function

--- odk_viewer/static/js/mapview.js
+++ odk_viewer/static/js/mapview.js
@@ -1,98 +1,76 @@
 var centerLatLng = new L.LatLng(center.lat, center.lng);
-var map;
 var defaultZoom = 8;
+var mapId = 'map_canvas';
+var map;
+var mapMarkerIcon = L.Icon.extend({
+    iconUrl: '/static/images/marker-solid-24.png',
+    shadowUrl: null,
+    iconSize: new L.Point(24, 24),
+    shadowSize: null,
+    iconAnchor: new L.Point(12, 24),
+    popupAnchor: new L.Point(0,-24)
+});
 
 function initialize() {
-
     // mapbox streets formhub tiles
     var url = 'http://a.tiles.mapbox.com/v3/modilabs.map-hgm23qjf.jsonp';
 
-    var mapMarkerIcon = L.Icon.extend({
-	iconUrl: '/static/images/marker-solid-24.png',
-	shadowUrl: null,
-	iconSize: new L.Point(24, 24),
-	shadowSize: null,
-	iconAnchor: new L.Point(12, 24),
-	popupAnchor: new L.Point(0,-24)
-    });
+    // Make a new Leaflet map in your container div
+    map = new L.Map(mapId).setView(centerLatLng, defaultZoom);
 
     // Get metadata about the map from MapBox
-    wax.tilejson(url,function(tilejson) {
-
-	var mapboxstreet = new wax.leaf.connector(tilejson);          
-
-      // Make a new Leaflet map in your container div
-        var map = new L.Map('map_canvas')  // container's id="mapbox"
-          
-            .setView(centerLatLng, defaultZoom)
-
+    wax.tilejson(url, function(tilejson) {
+        var mapboxstreet = new wax.leaf.connector(tilejson);
         // Add MapBox Streets as a base layer
-	
-            //.addLayer(new wax.leaf.connector(tilejson));      
-	    .addLayer(mapboxstreet);
-
-	//	var googleSat = new L.Google();
-	//	map.addLayer(googleSat);
-
-	map.addControl(new L.Control.Layers({'MapBox Streets':mapboxstreet}))
-
-/*
-
-Definetly the better way to do this is via GeoJSON in the future
-
-
-// Define a geojson data layer
-    var geojsonLayer = new L.GeoJSON(data, {
-	pointToLayer: function (latlng) {
-	    return new L.Marker(latlng, {
-		icon: new mapMarkerIcon()
-	    });
-	}      
+        //.addLayer(new wax.leaf.connector(tilejson));
+        map.addLayer(mapboxstreet);
+        //	var googleSat = new L.Google();
+        //	map.addLayer(googleSat);
+        map.addControl(new L.Control.Layers({'MapBox Streets':mapboxstreet}))
+        addPoints();
     });
-*/
+}
 
-
+function addPoints() {
     var latLngArray = new Array();
     for (var i=0; i<points.length; i=i+1) {
         // use a self executing function to create a new scope in each
         // iteration of the loop.
         latLngArray.push((function(){
-
             var point = new L.LatLng(points[i].lat, points[i].lng);
 
-	    // for marker
+            // for circle marker
             //var marker = new L.CircleMarker(point, {
             //    'radius': 6 
             //});
-	    var marker = new L.Marker(point, {icon: new mapMarkerIcon()});
+            var marker = new L.Marker(point, {icon: new mapMarkerIcon()});
 
             var instance = points[i].instance;
 
             // TODO: remove hard coded url
             var url = "/odk_viewer/survey/" + instance.toString() + "/";
             // open a loading popup so the user knows something is happening
-            //a = marker.bindPopup('Loading...');//.openPopup();
+            marker.bindPopup('Loading...').openPopup();
 
-           // bind open popup to marker's click event
-
+            // bind open popup to marker's click event
 
             marker.on('click', function(e){
                 var targetMarker = e.target;
 
-		/*  for marker logic
+                /*  for circle marker logic
                 var popup = new L.Popup({
                     'maxWidth': 500,
                 });
                 latlng = e.latlng;
                 latlng.lat += 1;
                 popup.setLatLng(latlng);
-		*/
+                */
 
- 
                $.get(url).done(function(data){
-		   targetMarker.bindPopup(data,{'maxWidth': 500}).openPopup();
-		   // popup.setContent(data);
-		   //  map.openPopup(popup);
+                    targetMarker.bindPopup(data,{'maxWidth': 500}).openPopup();
+                    // for circle marker
+                    // popup.setContent(data);
+                    //  map.openPopup(popup);
                 });
             });
 
@@ -105,6 +83,4 @@ Definetly the better way to do this is via GeoJSON in the future
         var latlngbounds = new L.LatLngBounds(latLngArray);
         map.fitBounds(latlngbounds);
    }
-
-    });
 }
