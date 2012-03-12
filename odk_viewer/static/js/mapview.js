0e992114792a6ac43c70245ac8cb381d6275ee7a
mberg <mlberg@gmail.com>
2012-03-12 16:24:56 -0400

    changed to MapBox streets from CloudMade and new icon marker

--- odk_viewer/static/js/mapview.js
+++ odk_viewer/static/js/mapview.js
@@ -1,23 +1,71 @@
-var centerLatlng = new L.LatLng(center.lat, center.lng);
+var centerLatLng = new L.LatLng(center.lat, center.lng);
 var map;
 var defaultZoom = 8;
+
 function initialize() {
-    map = new L.Map('map_canvas');
-    var cloudmadeTileLayer = new L.TileLayer('http://{s}.tile.cloudmade.com/b1e5699de0e74928959c89bf0f777a5b/997/256/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-        maxZoom: 18
+
+    // mapbox streets formhub tiles
+    var url = 'http://a.tiles.mapbox.com/v3/modilabs.map-hgm23qjf.jsonp';
+
+    var mapMarkerIcon = L.Icon.extend({
+	iconUrl: '/static/images/marker-solid-24.png',
+	shadowUrl: null,
+	iconSize: new L.Point(24, 24),
+	shadowSize: null,
+	iconAnchor: new L.Point(12, 24),
+	popupAnchor: new L.Point(0,-24)
     });
-    map.setView(centerLatlng, defaultZoom).addLayer(cloudmadeTileLayer);
+
+    // Get metadata about the map from MapBox
+    wax.tilejson(url,function(tilejson) {
+
+	var mapboxstreet = new wax.leaf.connector(tilejson);          
+
+      // Make a new Leaflet map in your container div
+        var map = new L.Map('map_canvas')  // container's id="mapbox"
+          
+            .setView(centerLatLng, defaultZoom)
+
+        // Add MapBox Streets as a base layer
+	
+            //.addLayer(new wax.leaf.connector(tilejson));      
+	    .addLayer(mapboxstreet);
+
+	//	var googleSat = new L.Google();
+	//	map.addLayer(googleSat);
+
+	map.addControl(new L.Control.Layers({'MapBox Streets':mapboxstreet}))
+
+/*
+
+Definetly the better way to do this is via GeoJSON in the future
+
+
+// Define a geojson data layer
+    var geojsonLayer = new L.GeoJSON(data, {
+	pointToLayer: function (latlng) {
+	    return new L.Marker(latlng, {
+		icon: new mapMarkerIcon()
+	    });
+	}      
+    });
+*/
+
+
     var latLngArray = new Array();
     for (var i=0; i<points.length; i=i+1) {
         // use a self executing function to create a new scope in each
         // iteration of the loop.
         latLngArray.push((function(){
-            // create a marker on the map for this point
+
             var point = new L.LatLng(points[i].lat, points[i].lng);
-            var marker = new L.CircleMarker(point, {
-                'radius': 8 
-            });
+
+	    // for marker
+            //var marker = new L.CircleMarker(point, {
+            //    'radius': 6 
+            //});
+	    var marker = new L.Marker(point, {icon: new mapMarkerIcon()});
+
             var instance = points[i].instance;
 
             // TODO: remove hard coded url
@@ -25,18 +73,26 @@ function initialize() {
             // open a loading popup so the user knows something is happening
             //a = marker.bindPopup('Loading...');//.openPopup();
 
-            // bind open popup to marker's click event
+           // bind open popup to marker's click event
+
+
             marker.on('click', function(e){
                 var targetMarker = e.target;
+
+		/*  for marker logic
                 var popup = new L.Popup({
                     'maxWidth': 500,
                 });
                 latlng = e.latlng;
                 latlng.lat += 1;
                 popup.setLatLng(latlng);
-                $.get(url).done(function(data){
-                    popup.setContent(data);
-                    map.openPopup(popup);
+		*/
+
+ 
+               $.get(url).done(function(data){
+		   targetMarker.bindPopup(data,{'maxWidth': 500}).openPopup();
+		   // popup.setContent(data);
+		   //  map.openPopup(popup);
                 });
             });
 
@@ -49,4 +105,6 @@ function initialize() {
         var latlngbounds = new L.LatLngBounds(latLngArray);
         map.fitBounds(latlngbounds);
    }
+
+    });
 }
