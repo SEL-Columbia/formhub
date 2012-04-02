ae46d8550de37303b34f57f413001812c8bef57b
Ukang'a Dickson <ukanga@gmail.com>
2012-04-02 13:46:53 +0300

    pep8 friendly

--- main/google_export.py
+++ main/google_export.py
@@ -19,14 +19,14 @@ from main.views import home
 
 token = gdata.gauth.OAuth2Token(client_id=settings.GOOGLE_CLIENT_ID,
     client_secret=settings.GOOGLE_CLIENT_SECRET,
-    scope='https://docs.google.com/feeds/ https://spreadsheets.google.com/feeds/',
-    user_agent='formhub'
-)
+    scope=', '.join(['https://docs.google.com/feeds/',
+        'https://spreadsheets.google.com/feeds/']),
+    user_agent='formhub')
 
 redirect_uri = token.generate_authorize_url(
     redirect_uri=settings.GOOGLE_STEP2_URI,
-    access_type='offline', approval_prompt='force'
-)
+    access_type='offline', approval_prompt='force')
+
 
 @login_required
 def google_oauth2_request(request):
