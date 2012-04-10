7efadd719d31f439e4144f3eb2d63d2c9b2dee20
pld <peter@helioid.com>
2012-04-10 14:19:28 -0400

    remove the fluff

--- utils/middleware.py
+++ utils/middleware.py
@@ -2,7 +2,4 @@ import traceback
 
 class ExceptionLoggingMiddleware(object):
         def process_exception(self, request, exception):
-            import logging
-            print traceback.format_exc()
-            logging.debug(exception)
-            logging.debug(traceback.format_exc())
+            print(traceback.format_exc())
