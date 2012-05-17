71d76a65fa1341ec506533d1dc53f579adb14505
Tom <tom1mt@yahoo.com>
2012-05-17 17:10:58 +0300

    Added image geocoding

--- odk_logger/management/commands/create_image_thumbnails.py
+++ odk_logger/management/commands/create_image_thumbnails.py
@@ -3,28 +3,34 @@
 
 from django.core.management.base import BaseCommand
 from django.core.files.storage import get_storage_class
+from django.conf import settings
 from odk_logger.models.attachment import Attachment
 from utils.model_tools import queryset_iterator
-from utils.logger_tools import get_dimensions, resize, resize_local_env
+from utils.logger_tools import get_dimensions, resize, resize_local_env, \
+                                write_exif
 from utils.viewer_tools import get_path
+from utils.geotag import set_gps_location
 
 class Command(BaseCommand):
     help = "Creates thumbnails for all form images and stores them"
 
     def handle(self, *args, **kwargs):
         fs = get_storage_class('django.core.files.storage.FileSystemStorage')()
-        for att in queryset_iterator(Attachment.objects.all()):
+        for att in queryset_iterator(Attachment.objects.select_related('instance', 'instance__xform').all()):
+            write_exif(att)
             filename = att.media_file.name
             default_storage = get_storage_class()()
-            if not default_storage.exists(get_path(filename, '-med.')):
+            if not default_storage.exists(get_path(filename, 
+                                    settings.THUMB_CONF['small']['suffix'])):
                 try:
                     if default_storage.__class__ != fs.__class__:
                         resize(filename)
                     else:
                         resize_local_env(filename)
-                    if default_storage.exists(get_path(filename, '-med.')):
-                        print 'Thumbnail created for %s' % filename
+                    if default_storage.exists(get_path(filename, 
+                                    settings.THUMB_CONF['small']['suffix'])):
+                        print 'Thumbnails created for %s' % filename
                     else:
                         print 'Something didn\'t go right for %s' % filename
-                except Exception, e:
+                except (IOError, OSError), e:
                     print 'Error on %s: %s' % (filename, e)
