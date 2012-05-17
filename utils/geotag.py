937e7e3f1c9cd9ff9221c3996de3f57acaf4bc32
Tom <tom1mt@yahoo.com>
2012-05-17 17:09:47 +0300

    Geocodes photos

---
+++ utils/geotag.py
@@ -0,0 +1,45 @@
+import pyexiv2
+
+def to_deg(value, loc):
+    if value < 0:
+        loc_value = loc[0]
+    elif value > 0:
+        loc_value = loc[1]
+    else:
+        loc_value = ""
+    abs_value = abs(value)
+    deg = int(abs_value)
+    t1 = (abs_value-deg)*60
+    min = int(t1)
+    sec = round((t1 - min)* 60, 5)
+    return (deg, min, sec, loc_value)
+
+def set_gps_location(file_name, lat, lng):
+    """Adds GPS position as EXIF metadata
+
+Keyword arguments:
+file_name -- image file
+lat -- latitude (as float)
+lng -- longitude (as float)
+
+"""
+    lat_deg = to_deg(lat, ["S", "N"])
+    lng_deg = to_deg(lng, ["W", "E"])
+
+    # convert decimal coordinates into degrees, munutes and seconds
+    exiv_lat = (pyexiv2.Rational(lat_deg[0]*60+lat_deg[1],60),pyexiv2.Rational(lat_deg[2]*100,6000), pyexiv2.Rational(0, 1))
+    exiv_lng = (pyexiv2.Rational(lng_deg[0]*60+lng_deg[1],60),pyexiv2.Rational(lng_deg[2]*100,6000), pyexiv2.Rational(0, 1))
+
+    exiv_image = pyexiv2.ImageMetadata(file_name)
+    exiv_image.read()
+    exif_keys = exiv_image.exif_keys
+
+    exiv_image["Exif.GPSInfo.GPSLatitude"] = exiv_lat
+    exiv_image["Exif.GPSInfo.GPSLatitudeRef"] = lat_deg[3]
+    exiv_image["Exif.GPSInfo.GPSLongitude"] = exiv_lng
+    exiv_image["Exif.GPSInfo.GPSLongitudeRef"] = lng_deg[3]
+    exiv_image["Exif.Image.GPSTag"] = 654
+    exiv_image["Exif.GPSInfo.GPSMapDatum"] = "WGS-84"
+    exiv_image["Exif.GPSInfo.GPSVersionID"] = '2 0 0 0'
+
+    exiv_image.write()
