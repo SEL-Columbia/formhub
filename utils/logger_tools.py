0da402c0ee7b5069e1a1ebf3b6be95fb56c35745
Tom <tom1mt@yahoo.com>
2012-05-18 15:52:44 +0300

    Added geocode exif data writing

--- utils/logger_tools.py
+++ utils/logger_tools.py
@@ -7,6 +7,7 @@ import urllib2 as urllib
 from PIL import Image
 from cStringIO import StringIO
 import json
+import pyexiv2
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
@@ -18,6 +19,7 @@ from django.http import HttpResponse
 from odk_logger.models.xform import XLSFormError
 from utils.viewer_tools import get_path
 from pyxform.errors import PyXFormError
+from utils.geotag import set_gps_location
 
 
 def report_exception(subject, info, exc_info=None):
@@ -127,6 +129,19 @@ def _save_thumbnails(image, path, size, suffix, filename=None):
         fs = get_storage_class('django.core.files.storage.FileSystemStorage')()
         image.thumbnail(get_dimensions(image.size, size), Image.ANTIALIAS)
         image.save(get_path(path, suffix))
+        
+        # copy EXIF data
+        source_image = pyexiv2.ImageMetadata(fs.path(filename))
+        source_image.read()
+        dest_image = pyexiv2.ImageMetadata(get_path(path, suffix))
+        dest_image.read()
+        source_image.copy(dest_image)
+        
+        # set EXIF image size info to resized size
+        dest_image["Exif.Photo.PixelXDimension"] = image.size[0]
+        dest_image["Exif.Photo.PixelYDimension"] = image.size[1]
+        dest_image.write()
+        
         default_storage.save(get_path(filename, suffix), 
                                 fs.open(get_path(path, suffix)))
     else:
@@ -163,6 +178,7 @@ def resize_local_env(filename):
 
 
 def write_exif(attachment):
+    # get the geopoint fields
     types =json.loads(attachment.instance.xform.json)
     column = ''
     for x in types['children']:
@@ -174,22 +190,20 @@ def write_exif(attachment):
     if gps:
         lat, lng, alt, acc = gps.split()
         fs = get_storage_class('django.core.files.storage.FileSystemStorage')()
-        default_storage = get_storage_class()()
-        path = default_storage.path(att.media_file.name)
-        
+        print gps
         try:
+            default_storage = get_storage_class()()
             if default_storage.__class__ != fs.__class__:
-                default_storage = get_storage_class()()
-                path = default_storage.url(filename)
+                path = default_storage.url(attachment.media_file.name)
                 img_file = urllib.urlopen(path)
                 im = StringIO(img_file.read())
                 image = Image.open(im)
                 image.save(fs.path(attachment.media_file.name))
-                set_gps_location(path, float(lat), float(lng))
+                set_gps_location(fs.path(attachment.media_file.name), 
+                                            float(lat), float(lng))
                 default_storage.save(attachment.media_file.name, 
                                 fs.open(fs.path(attachment.media_file.name)))
             else:
-                default_storage = get_storage_class()()
                 path = default_storage.path(attachment.media_file.name)
                 set_gps_location(path, float(lat), float(lng))
         except (IOError, OSError), e:
