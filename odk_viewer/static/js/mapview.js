b5d15d291ca5744c2df006a1b6eabe16fe75d189
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-05-31 04:54:54 -0400

    more jshint; @dorey @larryweya code review needed.. is this commit a good idea?

--- odk_viewer/static/js/mapview.js
+++ odk_viewer/static/js/mapview.js
@@ -566,16 +566,17 @@ function getBootstrapFields()
     // we only want to load gps and select one data to begin with
     var fields = [];
     var idx, question;
+    if(!constants) console.log("ERROR: constants not found; please include main/static/js/formManagers.js"); 
     for(idx in formJSONMngr.selectOneQuestions)
     {
         question = formJSONMngr.selectOneQuestions[idx];
-        fields.push(question["name"]);
+        fields.push(question[constants.NAME]);
     }
 
     for(idx in formJSONMngr.geopointQuestions)
     {
         question = formJSONMngr.geopointQuestions[idx];
-        fields.push(question["name"]);
+        fields.push(question[constants.NAME]);
     }
     return fields;
 }
