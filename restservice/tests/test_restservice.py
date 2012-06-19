74224a0949396bd8cf1c984de88b287820bc3c4b
Ukang'a Dickson <ukanga@gmail.com>
2012-06-19 15:32:54 +0300

    tests

--- restservice/tests/test_restservice.py
+++ restservice/tests/test_restservice.py
@@ -1,6 +1,7 @@
 import os
 from main.tests.test_base import MainTestCase
 from odk_logger.models.xform import XForm
+from restservice.RestServiceInterface import RestServiceInterface
 from restservice.models import RestService
 
 class RestServiceTest(MainTestCase):
@@ -14,9 +15,18 @@ class RestServiceTest(MainTestCase):
         self._publish_xls_file(path)
         self.xform = XForm.objects.all().reverse()[0]
 
+    def _create_rest_service(self):
+        rs = RestService(service_url=self.service_url,
+            xform=self.xform, name=self.service_name)
+        rs.save()
+        self.restservice = rs
+
     def test_create_rest_service(self):
         count = RestService.objects.all().count()
-        rs = RestService(service_url=self.service_url,
-                        xform=self.xform, name=self.service_name)
-        rs.save()
+        self._create_rest_service()
         self.assertEquals(RestService.objects.all().count(), count + 1)
+
+    def  test_service_definition(self):
+        self._create_rest_service()
+        sv = self.restservice.get_service_definition()()
+        self.assertEqual(isinstance(sv, RestServiceInterface), True)
