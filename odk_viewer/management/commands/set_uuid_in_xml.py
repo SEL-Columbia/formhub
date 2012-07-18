a0c1e8a65df047f92b17c70bd79d49067033cd69
modilabs-starscream <modilabs.starscream@gmail.com>
2012-07-18 02:39:28 -0400

    management command to add uuid to all existing xml files

---
+++ odk_viewer/management/commands/set_uuid_in_xml.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from odk_viewer.models import DataDictionary
+from utils.model_tools import queryset_iterator
+
+class Command(BaseCommand):
+    help = "Insert UUID into XML of all existing XForms"
+
+    def handle(self, *args, **kwargs):
+        print '%d XForms to update' % DataDictionary.objects.count()
+        for i, dd in enumerate(queryset_iterator(DataDictionary.objects.all())):
+            if dd.xls:
+                dd._set_uuid_in_xml()
+                dd.save()
+            if (i + 1) % 1000 == 0:
+                print 'Updated %d XForms...' % i
