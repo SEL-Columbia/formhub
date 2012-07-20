631fe56fc44ebf1627a482a1ddff0ae8b9d39938
Ukang'a Dickson <ukanga@gmail.com>
2012-07-20 18:50:03 +0000

    code refactoring, dupes

--- main/google_export.py
+++ main/google_export.py
@@ -19,55 +19,36 @@ from main.models import  TokenStorageModel
 from main.views import home
 from utils.google import oauth2_token, get_refreshed_token, redirect_uri
 
-#@login_required
+
 def google_oauth2_request(request):
+    token = None
     if request.user.is_authenticated():
         try:
             ts = TokenStorageModel.objects.get(id=request.user)
         except TokenStorageModel.DoesNotExist:
             pass
         else:
-            stored_token = gdata.gauth.token_from_blob(ts.token)
-            print stored_token.refresh_token, stored_token.access_token
-            if stored_token.refresh_token is not None and\
-               stored_token.access_token is not None:
-                token.refresh_token = stored_token.refresh_token
-                working_token = refresh_access_token(oauth2_token, request.user)
-                docs_client = client.SpreadsheetsClient(source=oauth2_token.user_agent)
-                docs_client = working_token.authorize(docs_client)
-                docs_feed = docs_client.GetSpreadsheets()
-                _l = '<ul>'
-                for entry in docs_feed.entry:
-                    _l += '<li>%s</li>' % entry.title.text
-                    print entry.title.text
-                _l += '</ul>'
-                return HttpResponse(_l)
-        return HttpResponseRedirect(redirect_uri)
-    else:
-        try:
-            #ts = TokenStorageModel.objects.get(id=request.user)
-            ts = TokenStorageModel.objects.get(id=0)
-        except TokenStorageModel.DoesNotExist:
-            pass
-        else:
-            stored_token = gdata.gauth.token_from_blob(ts.token)
-            print stored_token.refresh_token, stored_token.access_token
-            if stored_token.refresh_token is not None and\
-               stored_token.access_token is not None:
-                token.refresh_token = stored_token.refresh_token
-                working_token = refresh_access_token(oauth2_token, request.user)
-                docs_client = client.SpreadsheetsClient(source=oauth2_token.user_agent)
-                docs_client = working_token.authorize(docs_client)
-                docs_feed = docs_client.GetSpreadsheets()
-                _l = '<ul>'
-                for entry in docs_feed.entry:
-                    _l += '<li>%s</li>' % entry.title.text
-                    print entry.title.text
-                _l += '</ul>'
-                return HttpResponse(_l)
-        return HttpResponseRedirect(redirect_uri)
+            token  = ts.token
+    elif request.session.get('access_token'):
+        token = request.session.get('access_token')
+    if token is not None:
+        stored_token = gdata.gauth.token_from_blob(token)
+        if stored_token.refresh_token is not None and\
+           stored_token.access_token is not None:
+            oauth2_token.refresh_token = stored_token.refresh_token
+            working_token = refresh_access_token(oauth2_token, request.user)
+            docs_client = client.SpreadsheetsClient(source=oauth2_token.user_agent)
+            docs_client = working_token.authorize(docs_client)
+            docs_feed = docs_client.GetSpreadsheets()
+            _l = '<ul>'
+            for entry in docs_feed.entry:
+                _l += '<li>%s</li>' % entry.title.text
+                print entry.title.text
+            _l += '</ul>'
+            return HttpResponse(_l)
+    return HttpResponseRedirect(redirect_uri)
 
-#@login_required
+
 def google_auth_return(request):
     if 'code' not in request.REQUEST:
         return HttpResponse(u"Invalid Request")
@@ -88,11 +69,13 @@ def google_auth_return(request):
 
 
 def refresh_access_token(token, user):
+    token = get_refreshed_token(token)
+    if not user.is_authenticated():
+        return token
     try:
         ts = TokenStorageModel.objects.get(id=user)
     except TokenStorageModel.DoesNotExist:
         ts = TokenStorageModel(id=user)
-    token = get_refreshed_token(token)
     ts.token = gdata.gauth.token_to_blob(token)
     ts.save()
     return token
