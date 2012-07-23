4522f9a2f34f9f0e678d51f14169d22d2332cc00
Ukang'a Dickson <ukanga@gmail.com>
2012-07-23 13:30:56 +0300

    pep8

--- odk_viewer/views.py
+++ odk_viewer/views.py
@@ -12,10 +12,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.files.storage import get_storage_class
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseForbidden,\
-         HttpResponseBadRequest, HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponseForbidden,\
+    HttpResponseRedirect, HttpResponseNotFound, HttpResponseBadRequest
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
+from django.utils.translation import ugettext_lazy as _
 from django.utils import simplejson
 from pyxform import Section, Question
 
@@ -30,7 +31,7 @@ from odk_viewer.pandas_mongo_bridge import XLSDataFrameBuilder,\
 from csv_writer import CsvWriter
 from xls_writer import XlsWriter
 from utils.logger_tools import response_with_mimetype_and_name,\
-         disposition_ext_and_date, round_down_geopoint
+    disposition_ext_and_date, round_down_geopoint
 from utils.viewer_tools import image_urls, image_urls_for_form
 from utils.user_auth import has_permission, get_xform_and_perms
 
@@ -53,32 +54,33 @@ def dd_for_params(id_string, owner, request):
         except ValueError:
             # bad format
             return [False,
-                HttpReponseBadRequest(
-                        'Start time format must be YY_MM_DD_hh_mm_ss')
-            ]
-        dd.surveys_for_export = lambda d: d.surveys.filter(
-                date_created__gte=start)
+                    HttpResponseBadRequest(
+                        _(u'Start time format must be YY_MM_DD_hh_mm_ss'))
+                    ]
+        dd.surveys_for_export = \
+            lambda d: d.surveys.filter(date_created__gte=start)
     if request.GET.get('end'):
         try:
             end = encode(request.GET['end'])
         except ValueError:
             # bad format
             return [False,
-                HttpReponseBadRequest(
-                        'End time format must be YY_MM_DD_hh_mm_ss')
-            ]
-        dd.surveys_for_export = lambda d: d.surveys.filter(
-                date_created__lte=end)
+                    HttpReponseBadRequest(
+                        _(u'End time format must be YY_MM_DD_hh_mm_ss'))
+                    ]
+        dd.surveys_for_export = \
+            lambda d: d.surveys.filter(date_created__lte=end)
     if start and end:
-        dd.surveys_for_export = lambda d: d.surveys.filter(
-                date_created__lte=end, date_created__gte=start)
+        dd.surveys_for_export = \
+            lambda d: d.surveys.filter(date_created__lte=end,
+                                       date_created__gte=start)
     return [True, dd]
 
 
 def parse_label_for_display(pi, xpath):
     label = pi.data_dictionary.get_label(xpath)
     if not type(label) == dict:
-        label = { 'Unknown': label }
+        label = {'Unknown': label}
     return label.items()
 
 
@@ -92,7 +94,7 @@ def map_view(request, username, id_string):
     owner = get_object_or_404(User, username=username)
     xform = get_object_or_404(XForm, id_string=id_string, user=owner)
     if not has_permission(xform, owner, request):
-        return HttpResponseForbidden('Not shared.')
+        return HttpResponseForbidden(_(u'Not shared.'))
     context = RequestContext(request)
     context.content_user = owner
     context.xform = xform
@@ -105,7 +107,8 @@ def map_view(request, username, id_string):
     center = {
         'lat': round_down_geopoint(average([p['lat'] for p in points])),
         'lng': round_down_geopoint(average([p['lng'] for p in points])),
-        }
+    }
+
     def round_down_point(p):
         return {
             'lat': round_down_geopoint(p['lat']),
@@ -114,10 +117,12 @@ def map_view(request, username, id_string):
         }
     context.center = json.dumps(center)
     context.form_view = True
-    context.jsonform_url = reverse(download_jsonform, \
-        kwargs={"username": username, "id_string":id_string})
-    context.mongo_api_url = reverse(main.views.api, \
-        kwargs={"username": username, "id_string": id_string})
+    context.jsonform_url = reverse(download_jsonform,
+                                   kwargs={"username": username,
+                                           "id_string": id_string})
+    context.mongo_api_url = reverse(main.views.api,
+                                    kwargs={"username": username,
+                                            "id_string": id_string})
     context.mapbox_layer = MetaData.mapbox_layer_upload(xform)
     return render_to_response('map.html', context_instance=context)
 
@@ -125,8 +130,9 @@ def map_view(request, username, id_string):
 # TODO: do a good job of displaying hierarchical data
 def survey_responses(request, instance_id):
     pi = get_object_or_404(ParsedInstance, instance=instance_id)
-    xform, is_owner, can_edit, can_view = get_xform_and_perms(\
-            pi.instance.user.username, pi.instance.xform.id_string, request)
+    xform, is_owner, can_edit, can_view = \
+        get_xform_and_perms(pi.instance.user.username,
+                            pi.instance.xform.id_string, request)
     # no access
     if not (xform.shared_data or can_view or
             request.session.get('public_link')):
@@ -142,23 +148,23 @@ def survey_responses(request, instance_id):
     xpaths = data_for_display.keys()
     xpaths.sort(cmp=pi.data_dictionary.get_xpath_cmp())
     label_value_pairs = [
-         (parse_label_for_display(pi, xpath),
+        (parse_label_for_display(pi, xpath),
          data_for_display[xpath]) for xpath in xpaths
     ]
     languages = label_value_pairs[-1][0]
     return render_to_response('survey.html', {
-            'label_value_pairs': label_value_pairs,
-            'image_urls': image_urls(pi.instance),
-            'languages': languages,
-            'default_language': languages[0][0]
-            })
+        'label_value_pairs': label_value_pairs,
+        'image_urls': image_urls(pi.instance),
+        'languages': languages,
+        'default_language': languages[0][0]
+    })
 
 
 def csv_export(request, username, id_string):
     owner = get_object_or_404(User, username=username)
     xform = get_object_or_404(XForm, id_string=id_string, user=owner)
     if not has_permission(xform, owner, request):
-        return HttpResponseForbidden('Not shared.')
+        return HttpResponseForbidden(_(u'Not shared.'))
     query = request.GET.get("query")
     csv_dataframe_builder = CSVDataFrameBuilder(username, id_string, query)
     temp_file = NamedTemporaryFile(suffix=".csv")
@@ -166,7 +172,7 @@ def csv_export(request, username, id_string):
     if request.GET.get('raw'):
         id_string = None
     response = response_with_mimetype_and_name('application/csv', id_string,
-        extension='csv')
+                                               extension='csv')
     temp_file.seek(0)
     response.write(temp_file.read())
     temp_file.seek(0, os.SEEK_END)
@@ -179,7 +185,7 @@ def xls_export(request, username, id_string):
     owner = get_object_or_404(User, username=username)
     xform = get_object_or_404(XForm, id_string=id_string, user=owner)
     if not has_permission(xform, owner, request):
-        return HttpResponseForbidden('Not shared.')
+        return HttpResponseForbidden(_(u'Not shared.'))
     query = request.GET.get("query")
     xls_df_builder = XLSDataFrameBuilder(username, id_string, query)
     temp_file = NamedTemporaryFile(suffix=".xls")
@@ -188,7 +194,7 @@ def xls_export(request, username, id_string):
     if request.GET.get('raw'):
         id_string = None
     response = response_with_mimetype_and_name('vnd.ms-excel', id_string,
-        extension='xls')
+                                               extension='xls')
     response.write(temp_file.read())
     temp_file.seek(0, os.SEEK_END)
     response['Content-Length'] = temp_file.tell()
@@ -200,7 +206,7 @@ def zip_export(request, username, id_string):
     owner = get_object_or_404(User, username=username)
     xform = get_object_or_404(XForm, id_string=id_string, user=owner)
     if not has_permission(xform, owner, request):
-        return HttpResponseForbidden('Not shared.')
+        return HttpResponseForbidden(_(u'Not shared.'))
     dd = DataDictionary.objects.get(id_string=id_string,
                                     user=owner)
     if request.GET.get('raw'):
@@ -220,28 +226,31 @@ def zip_export(request, username, id_string):
     if request.GET.get('raw'):
         id_string = None
     response = response_with_mimetype_and_name('zip', id_string,
-            file_path=tmp.name, use_local_filesystem=True)
+                                               file_path=tmp.name,
+                                               use_local_filesystem=True)
     return response
+
 
 def kml_export(request, username, id_string):
     # read the locations from the database
     context = RequestContext(request)
-    context.message="HELLO!!"
+    context.message = "HELLO!!"
     owner = get_object_or_404(User, username=username)
     xform = get_object_or_404(XForm, id_string=id_string, user=owner)
     if not has_permission(xform, owner, request):
-        return HttpResponseForbidden('Not shared.')
+        return HttpResponseForbidden(_(u'Not shared.'))
     dd = DataDictionary.objects.get(id_string=id_string,
                                     user=owner)
     pis = ParsedInstance.objects.filter(instance__user=owner,
-            instance__xform__id_string=id_string, lat__isnull=False,
-            lng__isnull=False)
+                                        instance__xform__id_string=id_string,
+                                        lat__isnull=False, lng__isnull=False)
     data_for_template = []
 
     labels = {}
 
     def cached_get_labels(xpath):
-        if xpath in labels.keys(): return labels[xpath]
+        if xpath in labels.keys():
+            return labels[xpath]
         labels[xpath] = dd.get_label(xpath)
         return labels[xpath]
 
@@ -252,25 +261,28 @@ def kml_export(request, username, id_string):
         xpaths.sort(cmp=pi.data_dictionary.get_xpath_cmp())
         label_value_pairs = [
             (cached_get_labels(xpath),
-            data_for_display[xpath]) for xpath in xpaths 
-                                     if not xpath.startswith(u"_")]
+             data_for_display[xpath]) for xpath in xpaths
+            if not xpath.startswith(u"_")]
         table_rows = []
         for key, value in label_value_pairs:
             table_rows.append('<tr><td>%s</td><td>%s</td></tr>' % (key, value))
         img_urls = image_urls(pi.instance)
         img_url = img_urls[0] if img_urls else ""
         data_for_template.append({
-                'name':id_string,
-                'id': pi.id,
-                'lat': pi.lat,
-                'lng': pi.lng,
-                'image_urls': img_urls,
-                'table': '<table border="1"><a href="#"><img width="210" class="thumbnail" src="%s" alt=""></a>%s</table>' % (img_url,''.join(table_rows))})
+            'name': id_string,
+            'id': pi.id,
+            'lat': pi.lat,
+            'lng': pi.lng,
+            'image_urls': img_urls,
+            'table': '<table border="1"><a href="#"><img width="210" '
+                     'class="thumbnail" src="%s" alt=""></a>%s'
+                     '</table>' % (img_url, ''.join(table_rows))})
     context.data = data_for_template
-    response = render_to_response("survey.kml",
-        context_instance=context,
-        mimetype="application/vnd.google-earth.kml+xml")
-    response['Content-Disposition'] = disposition_ext_and_date(id_string, 'kml')
+    response = \
+        render_to_response("survey.kml", context_instance=context,
+                           mimetype="application/vnd.google-earth.kml+xml")
+    response['Content-Disposition'] = \
+        disposition_ext_and_date(id_string, 'kml')
     return response
 
 
@@ -279,9 +291,10 @@ def google_xls_export(request, username, id_string):
     owner = get_object_or_404(User, username=username)
     xform = get_object_or_404(XForm, id_string=id_string, user=owner)
     if not has_permission(xform, owner, request):
-        return HttpResponseForbidden('Not shared.')
+        return HttpResponseForbidden(_(u'Not shared.'))
     valid, dd = dd_for_params(id_string, owner, request)
-    if not valid: return dd
+    if not valid:
+        return dd
     ddw = XlsWriter()
     tmp = NamedTemporaryFile(delete=False)
     ddw.set_file(tmp)
@@ -304,8 +317,8 @@ def google_xls_export(request, username, id_string):
         return HttpResponseRedirect(redirect_uri)
     else:
         stored_token = gdata.gauth.token_from_blob(ts.token)
-        if stored_token.refresh_token is not None and\
-           stored_token.access_token is not None:
+        if stored_token.refresh_token is not None and \
+                stored_token.access_token is not None:
             token.refresh_token = stored_token.refresh_token
             working_token = refresh_access_token(token, request.user)
             docs_client = gdata.docs.client.DocsClient(source=token.user_agent)
@@ -323,33 +336,37 @@ def data_view(request, username, id_string):
     owner = get_object_or_404(User, username=username)
     xform = get_object_or_404(XForm, id_string=id_string, user=owner)
     if not has_permission(xform, owner, request):
-        return HttpResponseForbidden('Not shared.')
+        return HttpResponseForbidden(_(u'Not shared.'))
 
     context = RequestContext(request)
-    context.mongo_api_url = reverse(main.views.api,\
-        kwargs={"username": username, "id_string": id_string})
-    context.jsonform_url = reverse(download_jsonform,\
-        kwargs={"username": username, "id_string":id_string})
+    context.mongo_api_url = reverse(main.views.api,
+                                    kwargs={"username": username,
+                                            "id_string": id_string})
+    context.jsonform_url = reverse(download_jsonform,
+                                   kwargs={"username": username,
+                                           "id_string": id_string})
     return render_to_response("data_view.html", context_instance=context)
 
 
 def attachment_url(request):
     media_file = request.GET.get('media_file')
-    #TODO: how to make sure we have the right media file, this assumes duplicates are the same file
+    # TODO: how to make sure we have the right media file,
+    # this assumes duplicates are the same file
     result = Attachment.objects.filter(media_file=media_file)[0:1]
     if result.count() == 0:
-        return HttpResponseNotFound('Attachment not found')
+        return HttpResponseNotFound(_(u'Attachment not found'))
     attachment = result[0]
     media_url = attachment.media_file.url
     return redirect(media_url)
 
+
 def instance(request, username, id_string):
-    xform, is_owner, can_edit, can_view = get_xform_and_perms(\
+    xform, is_owner, can_edit, can_view = get_xform_and_perms(
         username, id_string, request)
     # no access
     if not (xform.shared_data or can_view or
             request.session.get('public_link')):
-        return HttpResponseForbidden('Not shared.')
+        return HttpResponseForbidden(_(u'Not shared.'))
 
     return render_to_response('instance.html', {
         'username': username,
