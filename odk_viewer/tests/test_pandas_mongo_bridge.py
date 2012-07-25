6d280bac892ff6b41114b429b4e3b6366a3c347c
modilabs-bumblebee <modilabs.bumblebee@gmail.com>
2012-07-25 13:44:42 +0300

    Fixed indent typo and added test since all tests passed with the typo, added a force xlsx param to view

--- odk_viewer/tests/test_pandas_mongo_bridge.py
+++ odk_viewer/tests/test_pandas_mongo_bridge.py
@@ -154,8 +154,33 @@ class TestPandasMongoBridge(MainTestCase):
                 'id_string': self.xform.id_string
             })
         self.response = self.client.get(url)
+        self.assertEqual(self.response.status_code, 200)
         self.assertEqual(self.response["content-type"],\
                'application/vnd.openxmlformats')
+
+    def test_xlsx_export_for_repeats(self):
+        """
+        Make sure exports run fine when the xlsx file has multiple sheets
+        """
+        self._publish_xls_fixture_set_xform("new_repeats")
+        self._submit_fixture_instance("new_repeats", "01")
+        xls_builder = XLSDataFrameBuilder(username=self.user.username,
+                id_string=self.xform.id_string)
+        # test that the view returns an xlsx file instead
+        url = reverse(xls_export,
+            kwargs={
+                'username': self.user.username,
+                'id_string': self.xform.id_string
+            }
+        )
+        params = {
+            'xlsx': 'true' # force xlsx
+        }
+        self.response = self.client.get(url, params)
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response["content-type"],\
+               'application/vnd.openxmlformats')
+
 
     def test_csv_dataframe_export_to(self):
         self._publish_nested_repeats_form()
