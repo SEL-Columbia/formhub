2bb4bfd572649d6576b4c31caff0e1b4c6b945ae
modilabs-bumblebee <modilabs.bumblebee@gmail.com>
2012-07-31 17:54:28 +0300

    code reformart, use simplejson

--- restservice/services/generic_json.py
+++ restservice/services/generic_json.py
@@ -1,15 +1,29 @@
 import httplib2
+
+from django.utils import simplejson
+from odk_viewer.models import ParsedInstance
 from restservice.RestServiceInterface import RestServiceInterface
-import json
+
 
 class ServiceDefinition(RestServiceInterface):
     id = u'json'
 
     def send(self, url, instance):
-        post_data = json.dumps(instance.xform.id_string)
+        args = {
+            'username': instance.xform.user.username,
+            'id_string': instance.xform.id_string,
+            'query': '{"uuid": "%s"}' % instance.uuid,
+            'fields': None,
+            'sort': None
+        }
+        try:
+            cursor = ParsedInstance.query_mongo(**args)
+        except ValueError, e:
+            return
+        records = list(record for record in cursor)
+        post_data = simplejson.dumps(records)
         headers = {"Content-Type": "application/json"}
         http = httplib2.Http()
-        resp, content = http.request(uri=url, method='POST',headers=headers
+        resp, content = http.request(uri=url, method='POST',headers=headers,
                                      body=post_data)
-        
 
