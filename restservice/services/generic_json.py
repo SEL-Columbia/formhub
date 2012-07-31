0e5f117c8804ba02c04c5849360d849b7b226eb4
modilabs-bumblebee <modilabs.bumblebee@gmail.com>
2012-07-31 15:24:24 +0300

    correct json dumping

--- restservice/services/generic_json.py
+++ restservice/services/generic_json.py
@@ -6,7 +6,7 @@ class ServiceDefinition(RestServiceInterface):
     id = u'json'
 
     def send(self, url, instance):
-        post_data = dumps(instance.xform.id_string)
+        post_data = json.dumps(instance.xform.id_string)
         headers = {"Content-Type": "application/json"}
         http = httplib2.Http()
         resp, content = http.request(uri=url, method='POST',headers=headers
