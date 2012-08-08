2cd1c220bbd0e78106f270393949cf5e28fe3c67
Larry Weya <larryweya@gmail.com>
2012-08-08 12:03:59 +0300

    Updated remongo to make use of QuerySet.iterator function  - more memory efficient but still processor heavy [ci skip]

--- odk_viewer/management/commands/remongo.py
+++ odk_viewer/management/commands/remongo.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = "Insert all existing parsed instances into MongoDB"
 
     def handle(self, *args, **kwargs):
-        for i, pi in enumerate(queryset_iterator(ParsedInstance.objects.all())):
+        for i, pi in enumerate(ParsedInstance.objects.iterator()):
             pi.update_mongo()
             if (i + 1) % 1000 == 0:
                 print 'Updated %d records, flushing MongoDB...' % i
