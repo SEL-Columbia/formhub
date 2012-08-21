9e4b6780e81641ef9a06ffcc8a571bfc7da2bc23
modilabs-starscream <modilabs.starscream@gmail.com>
2012-08-21 17:11:20 -0400

    add template test for chart function

--- odk_viewer/tests/__init__.py
+++ odk_viewer/tests/__init__.py
@@ -2,6 +2,7 @@ from survey_view import TestSurveyView
 from mongo_data_output import TestMongoData
 from test_map_view import TestMapView
 from test_exports import TestExports
+from test_chart_view import TestChartView
 from test_data_view import TestDataView
 from test_instance_view import TestInstanceView
 from test_attachment_url import TestAttachmentUrl
---
+++ odk_viewer/tests/test_chart_view.py
@@ -0,0 +1,10 @@
+from django.core.urlresolvers import reverse
+
+from main.tests.test_base import MainTestCase
+from odk_logger.views import chart
+
+
+class TestChartView(MainTestCase):
+
+    def test_chart(self):
+        pass
