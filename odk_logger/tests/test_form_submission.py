6176279338517fd8a6cf36161891aeb5b4ab43cf
Ukang'a Dickson <ukanga@gmail.com>
2012-09-04 17:37:19 +0300

    test for authenticated submission

--- odk_logger/tests/test_form_submission.py
+++ odk_logger/tests/test_form_submission.py
@@ -67,3 +67,33 @@ class TestFormSubmission(MainTestCase):
         self.assertEqual(self.response.status_code, 202)
         #/fixtures/test_forms/survey_names
 
+    def test_authenticated_submissions(self):
+        """
+        xml_submission_file is the field name for the posted xml file.
+        """
+        xml_submission_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "../fixtures/tutorial/instances/tutorial_2012-06-27_11-27-53.xml"
+        )
+        # authenticate first
+        url = '/%s/submission' % self.user.username
+        extra = {
+            'REQUEST_METHOD': 'HEAD',
+        }
+
+        from django_digest.test import Client as DigestClient
+
+        client = DigestClient()
+        # request with no credentials
+        req = client.get(url, {}, **extra)
+        self.assertEqual(req.status_code, 401)
+        # apply credentials
+        client.set_authorization('bob', 'bob', 'Digest')
+        req = client.get(url, {}, **extra)
+        # if 204 authorization successfull, proceed
+        self.assertEqual(req.status_code, 204)
+        # submissions should use this authenticated client
+        self.anon = client
+        self._make_submission(xml_submission_file_path)
+        self.assertEqual(self.response.status_code, 201)
+
