d8e9f2e634beb38002436d77f767fb15ffb5aec7
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-09-05 10:40:45 -0400

    beginning to test formManagers.js but cant figure out how to deal with $.getJSON

--- js_tests/mocks/sample.mock.js
+++ js_tests/mocks/sample.mock.js
@@ -1,3 +1,15 @@
 var SampleMockData = {
     "key": "val"
 }
+var FormJSONData = [{
+    url : 
+        "http://formhub.org/north_ghana/forms/_08_Water_points_train3/form.json",
+    geoPointQuestions :
+        [ { hint: { English: "Capture the geocode standing on or next to the water point."}},
+          { label: { English: "A-1.1  Geocode" }},
+          { name: "water_point_geocode" },
+          { type: "geopoint"}],
+    supportedLanguages :
+        [ "English" ],
+}
+]
--- js_tests/specs/sample.spec.js
+++ js_tests/specs/sample.spec.js
@@ -1,5 +1,6 @@
 // Load mocks for this spec
 EnvJasmine.load(EnvJasmine.mocksDir + "sample.mock.js");
+EnvJasmine.load(EnvJasmine.jsDir + "main/static/js/formManagers.js");
 
 describe("Sample", function () {
     it("asserts that one plus one equals two", function () {
@@ -8,5 +9,31 @@ describe("Sample", function () {
 
     it("asserts that sample moock data is defined", function() {
         expect(SampleMockData).toBeDefined();
+    });
+});
+
+describe("FormJSON tests", function() {
+    FormJSONData.forEach(function(formJSONDatum) {
+        $.getJSON(formJSONDatum.url, function(data) {
+            console.log(data);
+        });
+        it ("checks that form json loads properly for " + formJSONDatum.url, function () {
+            that = this; 
+            that.callbackCalled = false;
+            var mockCallBack = function () { that.callbackCalled = true; }
+            var sampleFormJSONManager = new FormJSONManager(formJSONDatum.url, mockCallBack);
+            runs(function () {
+                expect(sampleFormJSONManager).toBeDefined();
+                expect(sampleFormJSONManager.callback).toBeDefined();
+                sampleFormJSONManager.loadFormJSON();
+            });
+            waitsFor(function () {
+                return this.callBackCalled;
+            }, "Can sometimes take a while to load form.json", 20000);
+            runs(function() {
+                expect(sampleFormJSONManager.getGeoPointQuestion()).toEqual(formJSONDatum.geoPointQuestions[0]);
+                expect(sampleFormJSONManager.supportedLanguages).toEqual(formJSONDatum.supportedLanguages);
+            });
+        });
     });
 });
