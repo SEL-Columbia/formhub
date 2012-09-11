0aa57302861ce8e476a8a4d01c446187d845c3df
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-09-11 17:01:07 -0400

    lots of js tests for formJSONManager; multilingual labels still need better testing

--- js_tests/specs/sample.spec.js
+++ js_tests/specs/sample.spec.js
@@ -1,33 +1,48 @@
 // Load mocks for this spec
 EnvJasmine.load(EnvJasmine.mocksDir + "sample.mock.js");
 EnvJasmine.load(EnvJasmine.jsDir + "main/static/js/formManagers.js");
-
-describe("Sample", function () {
-    it("asserts that one plus one equals two", function () {
-        expect(1 + 1 == 2).toEqual(true);
-    });
-
-    it("asserts that sample moock data is defined", function() {
-        expect(SampleMockData).toBeDefined();
-    });
-});
+EnvJasmine.load(EnvJasmine.jsDir + "main/static/js/underscore-min.js");
 
 describe("FormJSON tests", function() {
-    var sampleFormJSONManager = new FormJSONManager(FormJSONData.url);
+    var sampleFJM;
+    beforeEach(function () {
+        sampleFJM = new FormJSONManager(fjmData.url);
+        sampleFJM._init(fjmData.actualJSON);
+    });
     
     it ("checks that form json loads properly for init with no callback", function () {
         runs(function () {
-            expect(sampleFormJSONManager).toBeDefined();
-            expect(sampleFormJSONManager.callback).toBeUndefined();
+            expect(sampleFJM).toBeDefined();
+            expect(sampleFJM.callback).toBeUndefined();
         });
     });
-    
-    sampleFormJSONManager._init(FormJSONData.actualJSON);
     it ("checks that form json init parses geopoints and supportedLanguages ", function () {
         runs(function() {
-            expect(sampleFormJSONManager.geopointQuestions).toEqual(FormJSONData.geoPointQuestions);
-            expect(sampleFormJSONManager.getGeoPointQuestion()).toEqual(FormJSONData.geoPointQuestions[0]);
-            expect(sampleFormJSONManager.supportedLanguages).toEqual(FormJSONData.supportedLanguages);
+            expect(sampleFJM.getGeoPointQuestion()).toEqual(fjmData.geoPointQuestions[0]);
+            expect(sampleFJM.geopointQuestions).toEqual(fjmData.geoPointQuestions);
+            expect(sampleFJM.supportedLanguages).toEqual(fjmData.supportedLanguages);
+        });
+    });
+    it ("checks that form json init parses selectOneQuestionNames", function () {
+        runs(function() {
+            expect(_.pluck(sampleFJM.getSelectOneQuestions(), 'name')).toEqual(fjmData.selectOneQuestionNames);
+            expect(_.uniq(_.pluck(sampleFJM.getSelectOneQuestions(), 'type'))).toEqual(["select one"]);
+        });
+    });
+    it ("checks questions are returned by name, and choices are returned by question", function () {
+        runs(function() {
+            expect(sampleFJM.getQuestionByName('water_point_geocode')).toEqual(fjmData.geoPointQuestions[0]);
+            expect(sampleFJM.getChoices(sampleFJM.getQuestionByName('animal_point'))).toEqual(fjmData.choicesForAnimalPoint);
+        });
+    });
+    it ("checks that form json parses labels for multiple languages", function () {
+        runs(function() {
+            var geoQ = sampleFJM.getQuestionByName('water_point_geocode');
+            expect(sampleFJM.getMultilingualLabel(geoQ)).toEqual(fjmData.geoPointQuestions[0].label.English);
+            expect(sampleFJM.getMultilingualLabel(geoQ), "English").toEqual(fjmData.geoPointQuestions[0].label.English);
+            expect(sampleFJM.getMultilingualLabel(geoQ), "French").toEqual(fjmData.geoPointQuestions[0].label.English);
+            expect(sampleFJM.getMultilingualLabel(geoQ), "^&~#*(").toEqual(fjmData.geoPointQuestions[0].label.English);
+
         });
     });
 });
