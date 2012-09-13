f6fc8359bb4aec0aa680236666ed46af4481af9d
modilabs-starscream <modilabs.starscream@gmail.com>
2012-09-13 18:09:33 -0400

    added enketo request tests

--- main/tests/test_form_enter_data.py
+++ main/tests/test_form_enter_data.py
@@ -6,6 +6,11 @@ from main.views import set_perm, show
 from main.models import MetaData
 from django.conf import settings
 from urlparse import urlparse
+from time import time
+import re
+import urllib2
+import urllib
+import json
 
 class TestFormEnterData(MainTestCase):
 
@@ -22,11 +27,68 @@ class TestFormEnterData(MainTestCase):
             'id_string': self.xform.id_string
         })
 
+        #for enketo use only
+
     def _running_enketo(self):
         if hasattr(settings, 'ENKETO_URL') and \
             self._check_url(settings.ENKETO_URL):
             return True
         return False
+
+    def test_enketo_remote_server_responses(self):
+        time_stamp = time()
+        form_id = "test_%s" % re.sub(re.compile("\."),"_",str(time()))
+        server_url = "%s/%s" % (self.base_url,self.user.username)
+        enketo_url = '%slaunch/launchSurvey' % settings.ENKETO_URL
+
+        values = {
+            'format': 'json',
+            'form_id': form_id,
+            'server_url' : server_url
+        }
+        data = urllib.urlencode(values)
+        req = urllib2.Request(enketo_url, data)
+        try:
+            response = urllib2.urlopen(req)
+            response = json.loads(response.read())
+        except urllib2.URLError:
+            pass
+        return_url = response['url']
+        success = response['success']
+        self.assertTrue(success)
+        enketo_base_url = urlparse(settings.ENKETO_URL).netloc
+        return_base_url = urlparse(return_url).netloc
+        self.assertIn(enketo_base_url, return_base_url)
+
+        #second time
+        req2 = urllib2.Request(enketo_url, data)
+        try:
+            response2 = urllib2.urlopen(req2)
+            response2 = json.loads(response2.read())
+        except urllib2.URLError:
+            pass
+        return_url_2 = response2['url']
+        success2 = response2['success']
+        reason2 = response2['reason']
+        self.assertEqual(return_url, return_url_2)
+        self.assertFalse(success2)
+        self.assertEqual(reason2, "existing")
+
+        #error message
+        values['server_url']=""
+        data = urllib.urlencode(values)
+        req3 = urllib2.Request(enketo_url, data)
+        try:
+            response3 = urllib2.urlopen(req3)
+            response3 = json.loads(response3.read())
+        except urllib2.URLError:
+            pass
+        success3 = response3['success']
+        reason3 = response3['reason']
+        self.assertFalse(success3)
+        self.assertEqual(reason3, "empty")
+
+
 
     def test_running_enketo(self):
         exist = self._running_enketo()
@@ -47,6 +109,10 @@ class TestFormEnterData(MainTestCase):
         self.assertEqual(response.status_code, 403)
 
     def test_public_with_link_to_share_toggle_on(self):
+        #sharing behavior as of 09/13/2012:
+        #it requires both data_share and form_share both turned on
+        #in order to grant anon access to form uploading
+        #TODO: findout 'for_user': 'all' and what it means
         response = self.client.post(self.perm_url, {'for_user': 'all',
             'perm_type': 'link'})
         self.assertEqual(response.status_code, 302)
@@ -56,3 +122,12 @@ class TestFormEnterData(MainTestCase):
         response = self.anon.get(self.url)
         status_code = 302 if self._running_enketo() else 403
         self.assertEqual(response.status_code, status_code)
+
+    def test_enketo_post_return(self):
+        #TODO: create a test case with name testuser and 
+        #id_string test_{UNIXTIME} to send to enketo
+        #and get the response back. 
+        #1) first time, return success
+        #2) second time, return already exist
+        #3) wrong one, return error
+        pass
