9ea670a34de68e8c2283b2f2d0e679c3ccfd1634
Larry Weya <larryweya@gmail.com>
2012-09-17 18:43:51 +0300

    Added 3gp and wav to allowed media upload types

--- main/models/meta_data.py
+++ main/models/meta_data.py
@@ -110,8 +110,10 @@ class MetaData(models.Model):
     def media_upload(xform, data_file=None):
         data_type = 'media'
         if data_file:
+	    import ipdb; ipdb.set_trace()
             if data_file.content_type in ['image/jpeg', 'image/png',
-                                          'audio/mpeg']:
+                                          'audio/mpeg', 'video/3gpp',
+					  'audio/wav']:
                 media = MetaData(data_type=data_type, xform=xform,
                                  data_value=data_file.name,
                                  data_file=data_file,
