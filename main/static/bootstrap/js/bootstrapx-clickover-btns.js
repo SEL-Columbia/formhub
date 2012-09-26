9bd0d9319a15fa991bf585d9dcd44fc9cd54dae5
Larry Weya <larryweya@gmail.com>
2012-09-26 18:27:38 +0300

    View buttons on hover [ci skip]

--- main/static/bootstrap/js/bootstrapx-clickover-btns.js
+++ main/static/bootstrap/js/bootstrapx-clickover-btns.js
@@ -21,6 +21,27 @@
 
     constructor: ClickoverBtns
 
+    , cinit: function( type, element, options ) {
+        this.attr = {};
+
+        // choose random attrs instead of timestamp ones
+        this.attr.me = ((Math.random() * 10) + "").replace(/\D/g, '');
+        this.attr.click_event_ns = "click." + this.attr.me;
+
+        if (!options) options = {};
+
+        options.trigger = 'manual';
+
+        // call parent
+        this.init( type, element, options );
+
+        // setup our own handlers
+        this.$element.on( 'mouseenter', this.options.selector, $.proxy(this.clickery, this) );
+
+        // soon add click hanlder to body to close this element
+        // will need custom handler inside here
+    }
+
     ,show: function () {
         var $tip
             , inside
