4d4d84c350a6b5d52c4de8568ec8a6a4600bbbfc
ukanga <ukanga@gmail.com>
2012-09-28 17:06:06 +0300

    default value 1

--- stats/tasks.py
+++ stats/tasks.py
@@ -3,7 +3,7 @@ from stats.models import StatsCount
 
 
 @celery.task
-def stat_log(key, value):
+def stat_log(key, value=1):
     try:
         sc = StatsCount.objects.create(key=key, value=value)
     except ValueError:
