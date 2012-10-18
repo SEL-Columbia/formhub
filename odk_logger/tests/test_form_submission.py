48a2ac78d6240233e344e99e891e603db6d2896d
ukanga <ukanga@gmail.com>
2012-10-18 12:10:16 +0300

    pep8 cleanup

--- odk_logger/tests/test_form_submission.py
+++ odk_logger/tests/test_form_submission.py
@@ -8,11 +8,10 @@ from main.tests.test_base import MainTestCase
 from odk_logger.models import XForm, Instance
 from odk_logger.models.instance import InstanceHistory
 from odk_viewer.models.parsed_instance import GLOBAL_SUBMISSION_STATS,\
-         ParsedInstance
+    ParsedInstance
 from stats.models import StatsCount
 from odk_logger.xform_instance_parser import clean_and_parse_xml
-from utils.logger_tools import inject_instanceid
-from tempfile import NamedTemporaryFile
+
 
 class TestFormSubmission(MainTestCase):
 
@@ -38,7 +37,8 @@ class TestFormSubmission(MainTestCase):
     def test_form_post_to_missing_form(self):
         xml_submission_file_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
-            "../fixtures/tutorial/instances/tutorial_invalid_id_string_2012-06-27_11-27-53.xml"
+            "../fixtures/tutorial/instances/"
+            "tutorial_invalid_id_string_2012-06-27_11-27-53.xml"
         )
         self._make_submission(xml_submission_file_path)
         self.assertEqual(self.response.status_code, 404)
@@ -52,8 +52,8 @@ class TestFormSubmission(MainTestCase):
             os.path.dirname(os.path.abspath(__file__)),
             "../fixtures/tutorial/instances/tutorial_2012-06-27_11-27-53.xml"
         )
-        self._make_submission(xml_submission_file_path, add_uuid=True,
-                touchforms=True)
+        self._make_submission(
+            xml_submission_file_path, add_uuid=True, touchforms=True)
         self.assertEqual(self.response.status_code, 201)
 
     def test_duplicate_submissions(self):
@@ -83,7 +83,7 @@ class TestFormSubmission(MainTestCase):
             key=GLOBAL_SUBMISSION_STATS).aggregate(Sum('value'))
         self.assertIsNone(submission_count['value__sum'])
         self._publish_transportation_form()
-        self.xform  = XForm.objects.get(id_string='transportation_2011_07_25')
+        self.xform = XForm.objects.get(id_string='transportation_2011_07_25')
         self._make_submissions()
         submission_count = StatsCount.objects.filter(
             key=GLOBAL_SUBMISSION_STATS).aggregate(Sum('value'))
@@ -112,7 +112,7 @@ class TestFormSubmission(MainTestCase):
         """
         Test submissions that have been edited
         """
-        xml_submission_file_path= os.path.join(
+        xml_submission_file_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
             "..", "fixtures", "tutorial", "instances",
             "tutorial_2012-06-27_11-27-53_w_uuid.xml"
@@ -138,7 +138,7 @@ class TestFormSubmission(MainTestCase):
             InstanceHistory.objects.count(), num_instances_history)
         # check count of mongo instances after first submission
         cursor = ParsedInstance.query_mongo(**query_args)
-        self.assertEqual(cursor[0]['count'], num_mongo_instances + 1 )
+        self.assertEqual(cursor[0]['count'], num_mongo_instances + 1)
         # edited submission
         xml_submission_file_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
@@ -153,7 +153,7 @@ class TestFormSubmission(MainTestCase):
         self.assertEqual(
             InstanceHistory.objects.count(), num_instances_history + 1)
         cursor = ParsedInstance.query_mongo(**query_args)
-        self.assertEqual(cursor[0]['count'], num_mongo_instances + 1 )
+        self.assertEqual(cursor[0]['count'], num_mongo_instances + 1)
         # make sure we edited the mongo db record and NOT added a new row
         query_args['count'] = False
         cursor = ParsedInstance.query_mongo(**query_args)
