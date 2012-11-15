dbce050d3503fab0f3f68a5a4982e98f778056a7
Ukang'a Dickson <ukanga@gmail.com>
2012-11-15 10:48:41 +0300

    middleware for 405 errors

--- utils/middleware.py
+++ utils/middleware.py
@@ -1,6 +1,20 @@
 import traceback
 
+from django.http import HttpResponseNotAllowed
+from django.template import RequestContext
+from django.template import loader
+
+
 class ExceptionLoggingMiddleware(object):
 
     def process_exception(self, request, exception):
         print(traceback.format_exc())
+
+
+class HTTPResponseNotAllowedMiddleware(object):
+
+    def process_response(self, request, response):
+	if isinstance(response, HttpResponseNotAllowed):
+	    context = RequestContext(request)
+	    response.content = loader.render_to_string("405.html", context_instance=context)
+	return response
