7859be241c0ed82be165f1b3a6c9187953aa5b52
Larry Weya <larryweya@gmail.com>
2012-12-14 17:31:48 +0300

    Compile query params before count [ci skip]

--- main/static/js/backend.activity-api.js
+++ main/static/js/backend.activity-api.js
@@ -37,69 +37,68 @@ this.recline.Backend.ActivityAPI = this.recline.Backend.ActivityAPI || {};
     };
 
     my.query = function(queryObj, dataset){
-        var deferred, jqXHR, params;
+        var deferred, jqXHR, params = {};
         deferred = $.Deferred();
-        params = {
-            count: 1
-        };
+        // query params
+        var queryParam = {'$and': []};
+        if(queryObj.q){
+            var qParam = {$or: []};
+            _.each(fields, function(field){
+                var reParam = {};
+                reParam[field.id] = {$regex: queryObj.q, $options: "i"};
+                qParam['$or'].push(reParam);
+            });
+            //params['query'] = JSON.stringify(qParam);
+            queryParam['$and'].push(qParam);
+        }
+        // filters
+        if(queryObj.filters.length > 0)
+        {
+            var filterParam = {$and: []};
+            _.each(queryObj.filters, function(filter){
+                if(filter.type === "term")
+                {
+                    var filterObj = {};
+                    filterObj[filter.field] = filter.term;
+                    filterParam.$and.push(filterObj);
+                }
+                else if(filter.type === "range")
+                {
+                    var filterObj = {};
+                    filterObj[filter.field] = {$gte: filter.start, $lte: filter.stop};
+                    filterParam.$and.push(filterObj);
+                }
+            });
+            // make sure we have some filters before we add to our array
+            if(filterParam.$and.length > 0)
+            {
+                queryParam['$and'].push(filterParam.$and.length > 1?filterParam:filterParam.$and[0]);
+            }
+        }
+        if(queryParam.$and && queryParam.$and.length > 0)
+        {
+            params['query'] = queryParam.$and.length > 1?JSON.stringify(queryParam):JSON.stringify(queryParam.$and[0]);
+        }
+        // default sort
+        var sort = {field: "created_on", order: "desc"};
+        var sortDirs = {asc: 1, desc: -1};
+        if(queryObj.sort && queryObj.sort.length > 0)
+        {
+            sort = queryObj.sort[0];
+        }
+        var sortParam = {};
+        sortParam[sort.field] = sortDirs[sort.order];
+        params['sort'] = JSON.stringify(sortParam);
+        // do a count first
+        params['count'] = 1;
         jqXHR = $.getJSON(dataset.url, params);
         jqXHR.done(function(data) {
             var total;
             total = data[0].count;
-            params = {
-                start: queryObj.from,
-                limit: queryObj.size
-            };
-            // query params
-            var queryParam = {'$and': []};
-            if(queryObj.q){
-                var qParam = {$or: []};
-                _.each(fields, function(field){
-                    var reParam = {};
-                    reParam[field.id] = {$regex: queryObj.q, $options: "i"};
-                    qParam['$or'].push(reParam);
-                });
-                //params['query'] = JSON.stringify(qParam);
-                queryParam['$and'].push(qParam);
-            }
-            // filters
-            if(queryObj.filters.length > 0)
-            {
-                var filterParam = {$and: []};
-                _.each(queryObj.filters, function(filter){
-                    if(filter.type === "term")
-                    {
-                        var filterObj = {};
-                        filterObj[filter.field] = filter.term;
-                        filterParam.$and.push(filterObj);
-                    }
-                    else if(filter.type === "range")
-                    {
-                        var filterObj = {};
-                        filterObj[filter.field] = {$gte: filter.start, $lte: filter.stop};
-                        filterParam.$and.push(filterObj);
-                    }
-                });
-                // make sure we have some filters before we add to our array
-                if(filterParam.$and.length > 0)
-                {
-                    queryParam['$and'].push(filterParam.$and.length > 1?filterParam:filterParam.$and[0]);
-                }
-            }
-            if(queryParam.$and && queryParam.$and.length > 0)
-            {
-                params['query'] = queryParam.$and.length > 1?JSON.stringify(queryParam):JSON.stringify(queryParam.$and[0]);
-            }
-            // default sort
-            var sort = {field: "created_on", order: "desc"};
-            var sortDirs = {asc: 1, desc: -1};
-            if(queryObj.sort && queryObj.sort.length > 0)
-            {
-                var sort = queryObj.sort[0];
-            }
-            var sortParam = {};
-            sortParam[sort.field] = sortDirs[sort.order];
-            params['sort'] = JSON.stringify(sortParam);
+            // stop counting
+            delete(params.count);
+            params.start = queryObj.from;
+            params.limit = queryObj.size;
             jqXHR = $.getJSON(dataset.url, params);
             return jqXHR.done(function(data) {
                 return deferred.resolve({
