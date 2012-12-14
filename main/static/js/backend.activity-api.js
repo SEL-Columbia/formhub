b70b7b0d014ec226915e02a5d099a372d82a8ab5
Larry Weya <larryweya@gmail.com>
2012-12-14 16:53:14 +0300

    Implemented query params to the api

--- main/static/js/backend.activity-api.js
+++ main/static/js/backend.activity-api.js
@@ -50,6 +50,46 @@ this.recline.Backend.ActivityAPI = this.recline.Backend.ActivityAPI || {};
                 start: queryObj.from,
                 limit: queryObj.size
             };
+            // query params
+            var queryParam = {'$and': []};
+            if(queryObj.q){
+                var qParam = {$or: []};
+                _.each(fields, function(field){
+                    var reParam = {};
+                    reParam[field.id] = {$regex: queryObj.q, $options: "i"};
+                    qParam['$or'].push(reParam);
+                });
+                //params['query'] = JSON.stringify(qParam);
+                queryParam['$and'].push(qParam);
+            }
+            // filters
+            if(queryObj.filters.length > 0)
+            {
+                var filterParam = {$and: []};
+                _.each(queryObj.filters, function(filter){
+                    if(filter.type === "term")
+                    {
+                        var filterObj = {};
+                        filterObj[filter.field] = filter.term;
+                        filterParam.$and.push(filterObj);
+                    }
+                    else if(filter.type === "range")
+                    {
+                        var filterObj = {};
+                        filterObj[filter.field] = {$gte: filter.start, $lte: filter.stop};
+                        filterParam.$and.push(filterObj);
+                    }
+                });
+                // make sure we have some filters before we add to our array
+                if(filterParam.$and.length > 0)
+                {
+                    queryParam['$and'].push(filterParam.$and.length > 1?filterParam:filterParam.$and[0]);
+                }
+            }
+            if(queryParam.$and && queryParam.$and.length > 0)
+            {
+                params['query'] = queryParam.$and.length > 1?JSON.stringify(queryParam):JSON.stringify(queryParam.$and[0]);
+            }
             // default sort
             var sort = {field: "created_on", order: "desc"};
             var sortDirs = {asc: 1, desc: -1};
