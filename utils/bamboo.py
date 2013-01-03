739adc83accb93ed6c6da155ad60119ab6164241
renaud gaudin <rgaudin@gmail.com>
2013-01-03 11:58:21 +0000

    Fixed bamboo restservice (API changed). Now uses pybamboo

--- utils/bamboo.py
+++ utils/bamboo.py
@@ -3,6 +3,8 @@ import json
 import StringIO
 
 import requests
+from pybamboo.dataset import Dataset
+from pybamboo.connection import Connection
 
 from odk_viewer.pandas_mongo_bridge import (CSVDataFrameBuilder,
                                             NoRecordsFoundError)
@@ -22,30 +24,28 @@ def get_new_bamboo_dataset(xform):
 
     dataset_id = u''
 
-    url = '%(url_root)s/datasets' % {'url_root': get_bamboo_url(xform)}
-    
-
     try:
-        csv_data = get_csv_data(xform)
+        dataset = Dataset(connection=Connection(url=get_bamboo_url(xform)),
+                          content=get_csv_data(xform))
     except NoRecordsFoundError:
         return dataset_id
 
-    req = requests.post(url, files={'csv_file': csv_data.getvalue()})
+    if dataset.id:
+        return dataset.id
 
-    if req.status_code in (200, 201, 202):
-        try:
-            dataset_id = json.loads(req.text).get('id')
-        except:
-            pass
     return dataset_id
 
 
 def get_csv_data(xform):
-    
+
     csv_dataframe_builder = CSVDataFrameBuilder(xform.user.username,
                                                 xform.id_string, '')
     buff = StringIO.StringIO()
-    
+
     # might raise NoRecordsFoundError
     csv_dataframe_builder.export_to(buff)
+
+    if not buff.len:
+        raise NoRecordsFoundError
+
     return buff
