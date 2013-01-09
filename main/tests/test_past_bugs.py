6e5f708b995830dcf2bb36775612f1b86b8d57e2
Ukang'a Dickson <ukanga@gmail.com>
2013-01-09 13:25:56 +0300

    test for mixed case username submission

--- main/tests/test_past_bugs.py
+++ main/tests/test_past_bugs.py
@@ -1,6 +1,6 @@
 from test_process import TestSite
 from test_base import MainTestCase
-from odk_logger.models import XForm
+from odk_logger.models import XForm, Instance
 import os
 
 
@@ -24,8 +24,11 @@ class TestInputs(MainTestCase):
     def test_uniqueness_of_group_names_enforced(self):
         pre_count = XForm.objects.count()
         self._create_user_and_login()
-        response = self._publish_xls_file('fixtures/group_names_must_be_unique.xls')
-        self.assertTrue("There are two sections with the name group_names_must_be_unique." in response.content)
+        response = self._publish_xls_file(
+            'fixtures/group_names_must_be_unique.xls')
+        self.assertTrue(
+            "There are two sections with the name group_names_must_be_unique."
+            in response.content)
         self.assertEqual(XForm.objects.count(), pre_count)
 
     def test_mch(self):
@@ -34,5 +37,18 @@ class TestInputs(MainTestCase):
     def test_erics_files(self):
         for name in ['battery_life.xls',
                      'enumerator_weekly.xls',
-                     'Enumerator_Training_Practice_Survey.xls',]:
+                     'Enumerator_Training_Practice_Survey.xls']:
             self._publish_xls_file(os.path.join('fixtures', 'bug_fixes', name))
+
+
+class TestSubmissionBugs(MainTestCase):
+
+    def test_submission_with_mixed_case_username(self):
+        self._publish_transportation_form()
+        s = self.surveys[0]
+        count = Instance.objects.count()
+        self._make_submission(
+            os.path.join(
+                self.this_directory, 'fixtures',
+                'transportation', 'instances', s, s + '.xml'), 'BoB')
+        self.assertEqual(Instance.objects.count(), count + 1)
