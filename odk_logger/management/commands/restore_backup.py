b092f095b3c0fc0f2e41c890428aabc88ac79347
Larry Weya <larryweya@gmail.com>
2013-02-06 09:44:56 +0300

    Commands and functions to create and restore from zip backups

---
+++ odk_logger/management/commands/restore_backup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext_lazy, ugettext as _
+from django.contrib.auth.models import User
+from utils.backup_tools import restore_backup_from_zip
+
+class Command(BaseCommand):
+    args = 'username id_string input_file'
+    help = ugettext_lazy("Restore a zip backup of a form and all its"
+                         " submissions")
+
+    def handle(self, *args, **options):
+        try:
+            username = args[0]
+        except IndexError:
+            raise CommandError(_("You must provide the username to publish the"
+                                 " form to."))
+            # make sure user exists
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise CommandError(_("The user '%s' does not exist.") % username)
+
+        try:
+            input_file = args[1]
+        except IndexError:
+            raise CommandError(_("You must provide the path to restore from."))
+        else:
+            input_file = os.path.realpath(input_file)
+
+        num_instances, num_restored = restore_backup_from_zip(
+            input_file, username)
+        sys.stdout.write("Restored %d of %d submissions\n" %
+                         (num_restored, num_instances))
--- odk_logger/tests/test_backup_tools.py
+++ odk_logger/tests/test_backup_tools.py
@@ -1,23 +1,28 @@
 import os
+import tempfile
+import shutil
 from datetime import datetime
 from django.conf import settings
+from odk_logger.models import Instance
 from odk_logger.import_tools import django_file
 from main.tests.test_base import MainTestCase
 from odk_logger.import_tools import create_instance
-from utils.backup_tools import _date_created_from_filename
+from utils.backup_tools import _date_created_from_filename,\
+    create_zip_backup, restore_backup_from_zip
 
 
 class TestBackupTools(MainTestCase):
     def setUp(self):
-        MainTestCase.setUp(self)
-        self._publish_xls_file(
+        super(TestBackupTools, self).setUp()
+        self._publish_xls_file_and_set_xform(
             os.path.join(
                 settings.PROJECT_ROOT,
                 "odk_logger", "fixtures", "test_forms", "tutorial.xls"))
 
     def test_date_created_override(self):
         """
-        Test that passing a date_created_override when creating and instance will set our date as the date_created
+        Test that passing a date_created_override when creating and instance
+        will set our date as the date_created
         """
         xml_file_path = os.path.join(
             settings.PROJECT_ROOT, "odk_logger", "fixtures", "tutorial",
@@ -42,3 +47,44 @@ class TestBackupTools(MainTestCase):
         filename = "%s-%d.xml" % (date_str, 1)
         generated_date_created = _date_created_from_filename(filename)
         self.assertEqual(generated_date_created, date_created)
+
+    def test_backup_then_restore_from_zip(self):
+        self._publish_transportation_form()
+        query_set = Instance.objects.filter(
+            xform=self.xform)
+        initial_instance_count = Instance.objects.filter(
+            xform=self.xform).count()
+
+        # make submissions
+        for i in range(len(self.surveys)):
+            self._submit_transport_instance(i)
+
+        instance_count =  Instance.objects.filter(
+            xform=self.xform).count()
+        self.assertEqual(
+            instance_count, initial_instance_count + len(self.surveys))
+
+        # make a backup
+        temp_dir = tempfile.mkdtemp()
+        zip_file = open(os.path.join(temp_dir, "backup.zip"), "wb")
+        zip_file.close()
+        create_zip_backup(zip_file.name, self.user, self.xform)
+
+        # delete all the instances
+        for instance in Instance.objects.filter(xform=self.xform):
+            instance.delete()
+
+        instance_count = Instance.objects.filter(
+            xform=self.xform).count()
+        # remove temp dir tree
+        self.assertEqual(instance_count, 0)
+
+        # restore instances
+        self.assertTrue(os.path.exists(zip_file.name))
+        restore_backup_from_zip(
+            zip_file.name, self.user.username)
+        instance_count = Instance.objects.filter(
+            xform=self.xform).count()
+        # remove temp dir tree
+        self.assertEqual(instance_count, len(self.surveys))
+        shutil.rmtree(temp_dir)
