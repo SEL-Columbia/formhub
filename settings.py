20fa7b7eb57dccb2009d547de2ab4c9176925e9d
Larry Weya <larryweya@gmail.com>
2013-03-01 21:05:29 +0300

    Used old test runner just to be sure

--- settings.py
+++ settings.py
@@ -279,7 +279,7 @@ if TESTING_MODE:
     # need to have CELERY_ALWAYS_EAGER True and BROKER_BACKEND as memory to run taks immediately while testing
     CELERY_ALWAYS_EAGER = True
     BROKER_BACKEND = 'memory'
-    #TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
+    TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
 else:
     MEDIA_ROOT  = os.path.join(PROJECT_ROOT, 'media/')
     MONGO_DB = _MONGO_CONNECTION[MONGO_DB_NAME]
