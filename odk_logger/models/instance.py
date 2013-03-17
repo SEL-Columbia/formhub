9b6c0ad1d27065acd0689e755eeb5f5e5debec72
Ukang'a Dickson <ukanga@gmail.com>
2013-03-17 18:24:00 +0300

    pep8 cleanup

--- odk_logger/models/instance.py
+++ odk_logger/models/instance.py
@@ -18,6 +18,7 @@ from django.utils.translation import ugettext_lazy, ugettext as _
 class FormInactiveError(Exception):
     def __unicode__(self):
         return _("Form is inactive")
+
     def __str__(self):
         return unicode(self).encode('utf-8')
 
