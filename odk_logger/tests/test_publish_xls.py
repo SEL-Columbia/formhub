2f619026f7da9d50a59a61e68475d0759a692db7
Ukang'a Dickson <ukanga@gmail.com>
2013-03-17 14:42:02 +0300

    test for line break

--- odk_logger/tests/test_publish_xls.py
+++ odk_logger/tests/test_publish_xls.py
@@ -1,4 +1,6 @@
 import os
+import codecs
+
 from django.core.management import call_command
 from main.tests.test_base import MainTestCase
 from odk_logger.models.xform import XForm
@@ -43,3 +45,24 @@ class TestPublishXLS(MainTestCase):
         is_updated_form = len([e.name for e in data_dictionary.survey_elements
                                if e.name == u'preferred_means']) > 0
         self.assertTrue(is_updated_form)
+
+    def test_line_break_in_variables(self):
+        xls_file_path = os.path.join(
+            self.this_directory, "fixtures", 'exp_line_break.xlsx')
+        xml_file_path = os.path.join(
+            self.this_directory, "fixtures", 'exp_line_break.xml')
+        test_xml_file_path = os.path.join(
+            self.this_directory, "fixtures", 'test_exp_line_break.xml')
+        self._publish_xls_file(xls_file_path)
+        xforms = XForm.objects.filter(id_string='exp_line_break')
+        self.assertTrue(xforms.count() > 0)
+        xform = xforms[0]
+        xform.xml = xform.xml.replace(xform.uuid, '663123a849e54bffa8f9832ef016bfac')
+        xform.save()
+        f = codecs.open(test_xml_file_path, 'w', encoding="utf-8")
+        f.write(xform.xml)
+        f.close()
+        with codecs.open(xml_file_path, 'rb', encoding="utf-8") as expected_file:
+            with codecs.open(test_xml_file_path, 'rb', encoding="utf-8") as actual_file:
+                self.assertMultiLineEqual(expected_file.read(), actual_file.read())
+        os.remove(test_xml_file_path)
