b9d5e7f14d3cac487b9cc506eb382f43f27e63dd
Ukang'a Dickson <ukanga@gmail.com>
2013-04-07 16:49:31 +0530

    test for create_image_thumbnails command

--- odk_logger/tests/test_models.py
+++ odk_logger/tests/test_models.py
@@ -1,7 +1,10 @@
 import os
 
+from datetime import datetime
+
 from django.core.files.base import File
 from django.core.files.storage import default_storage
+from django.core.management import call_command
 
 from odk_logger.models import Attachment, Instance
 
@@ -19,16 +22,16 @@ class AttachmentTest(MainTestCase):
         media_file = os.path.join(
             self.this_directory, 'fixtures',
             'transportation', 'instances', self.surveys[0], self.media_file)
-        instance = Instance.objects.all()[0]
+        self.instance = Instance.objects.all()[0]
         self.attachment = Attachment.objects.create(
-            instance=instance, media_file=File(open(media_file), media_file))
+            instance=self.instance,
+            media_file=File(open(media_file), media_file))
 
     def test_mimetype(self):
         self.assertEqual(self.attachment.mimetype, 'image/jpeg')
 
     def test_thumbnails(self):
-        instance = Instance.objects.all()[0]
-        for attachment in Attachment.objects.filter(instance=instance):
+        for attachment in Attachment.objects.filter(instance=self.instance):
             url = image_url(attachment, 'small')
             filename = attachment.media_file.name.replace('.jpg', '')
             thumbnail = '%s-small.jpg' % filename
@@ -38,4 +41,25 @@ class AttachmentTest(MainTestCase):
                 thumbnail = '%s-%s.jpg' % (filename, size)
                 self.assertTrue(
                     default_storage.exists(thumbnail))
+                default_storage.delete(thumbnail)
+
+    def test_create_thumbnails_command(self):
+        call_command("create_image_thumbnails")
+        for attachment in Attachment.objects.filter(instance=self.instance):
+            filename = attachment.media_file.name.replace('.jpg', '')
+            for size in ['small', 'medium', 'large']:
+                thumbnail = '%s-%s.jpg' % (filename, size)
+                self.assertTrue(
+                    default_storage.exists(thumbnail))
+        check_datetime = datetime.now()
+        # replace or regenerate thumbnails if they exist
+        call_command("create_image_thumbnails", force=True)
+        for attachment in Attachment.objects.filter(instance=self.instance):
+            filename = attachment.media_file.name.replace('.jpg', '')
+            for size in ['small', 'medium', 'large']:
+                thumbnail = '%s-%s.jpg' % (filename, size)
+                self.assertTrue(
+                    default_storage.exists(thumbnail))
+                self.assertTrue(
+                    default_storage.modified_time(thumbnail) > check_datetime)
                 default_storage.delete(thumbnail)
