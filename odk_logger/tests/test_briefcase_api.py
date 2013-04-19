1996521fb74ecda439c8ae8c17d3b8e8d49bdd42
Ukang'a Dickson <ukanga@gmail.com>
2013-04-19 12:14:44 +0300

    briefcase api: view/submissionList - handle cursor and numEntries queries test

--- odk_logger/tests/test_briefcase_api.py
+++ odk_logger/tests/test_briefcase_api.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 from main.tests.test_base import MainTestCase
 
 from odk_logger.views import view_submission_list
+from odk_logger.models import Instance
 
 
 class TestBriefcaseAPI(MainTestCase):
@@ -14,12 +15,13 @@ class TestBriefcaseAPI(MainTestCase):
         self._create_user_and_login()
         self._publish_transportation_form()
         self._make_submissions()
+        self._submission_list_url = reverse(
+            view_submission_list,
+            kwargs={'username': self.user.username})
 
     def test_view_submissionList(self):
         response = self.client.get(
-            reverse(
-                view_submission_list,
-                kwargs={'username': self.user.username}),
+            self._submission_list_url,
             data={'formId': self.xform.id_string})
         self.assertEqual(response.status_code, 200)
         submission_list_path = os.path.join(
@@ -28,3 +30,25 @@ class TestBriefcaseAPI(MainTestCase):
         with codecs.open(submission_list_path, 'rb', encoding='utf-8') as f:
             expected_submission_list = f.read()
             self.assertEqual(response.content, expected_submission_list)
+
+    def test_view_submissionList_numEntries(self):
+        params = {'formId': self.xform.id_string}
+        params['numEntries'] = 2
+        last_index = Instance.objects.filter(xform=self.xform)[:2][1].pk
+        for index in range(1, 3):
+            response = self.client.get(
+                self._submission_list_url,
+                data=params)
+            self.assertEqual(response.status_code, 200)
+            # set cursor for second request
+            params['cursor'] = last_index
+            submission_list_path = os.path.join(
+                self.this_directory, 'fixtures', 'transportation',
+                'view', 'submissionList-%s.xml' % index)
+            with codecs.open(submission_list_path, encoding='utf-8') as f:
+                expected_submission_list = f.read()
+                expected_submission_list = \
+                    expected_submission_list.replace(
+                        '{{resumptionCursor}}', '%s' % last_index)
+                self.assertEqual(response.content, expected_submission_list)
+            last_index += 2
