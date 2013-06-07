e506b90dfc1dec2f375c83401b2391b28ae9e5e0
Ukang'a Dickson <ukanga@gmail.com>
2013-06-07 17:30:15 +0300

    Larry & Dickson | remove user form an organization

--- api/models/models.py
+++ api/models/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, Permission, Group
-from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User, Group
 
 from main.models import UserProfile
 
@@ -27,34 +26,50 @@ class OrganizationProfile(UserProfile):
     def save(self, *args, **kwargs):
         super(OrganizationProfile, self).save(*args, **kwargs)
 
+    def remove_user_from_organization(self, user):
+        """Remove's a user from all teams/groups in the organization
+        """
+        for group in user.groups.filter('%s#' % self.user.username):
+            user.groups.remove(group)
+
 
 class Project(models.Model):
-    pass
+    """
+    - @name
+    - belongs to an organization
+    - @creator
+    - @teams - permissions?
+    - its own team?
+
+    Larry owns modlabs organization
+    Larry creates a project 'atasoils' to modilabs
+    - create a project  ata_soils
+        - name = ata_soils
+        - creator = larry
+        - who has access to atasoils?
+        - do we add a team to atasoils?
+        - do we have a atasoils project team auto created?
+    """
 
 
 class Team(Group):
     """
-    - They belong to an organization
-    - They have members
-    - They have access to projects
-    - should this be a django group - permissions?
-    - @name
-    - @organization
-    - @members
-    - @projects
+    TODO: documentation
+    TODO: Whenever a member is removed from members team,
+          we  should remove them from all teams and projects
+          within the organization.
     """
-
     class Meta:
         app_label = 'api'
 
     OWNER_TEAM_NAME = "Owners"
 
+    organization = models.ForeignKey(User)
+    projects = models.ManyToManyField(Project)
+
     def __unicode__(self):
         # return a clear group name without username to user for viewing
         return self.name.split('#')[1]
-
-    organization = models.ForeignKey(User)
-    projects = models.ManyToManyField(Project)
 
     def save(self, *args, **kwargs):
         # allow use of same name in different organizations/users
