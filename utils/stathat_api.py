bd23721508a55250e0445c5ba8ec5a0edc5ca119
Larry Weya <larryweya@gmail.com>
2013-06-13 17:27:58 +0300

    catch HTTP errors thrown by stathat api and report to admins - fixes #1152

--- utils/stathat_api.py
+++ utils/stathat_api.py
@@ -7,6 +7,7 @@ from django.core.mail import mail_admins
 def stathat_count(stat, count=1):
     if hasattr(settings, 'STATHAT_EMAIL'):
         stathat = StatHat()
+
         try:
             result = stathat.ez_post_count(settings.STATHAT_EMAIL, stat, count)
         except HTTPError as e:
