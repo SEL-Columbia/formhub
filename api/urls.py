727db2750118e909d216c65e7acfe8127c942d8f
Ukang'a Dickson <ukanga@gmail.com>
2013-06-20 16:12:02 +0300

    remove api urls for profiles and groups, and related view sets

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,4 @@ from api import views as api_views
 
 
 router = routers.DefaultRouter()
-router.register(r'users', api_views.UserViewSet)
-router.register(r'profiles', api_views.UserProfileViewSet)
-router.register(r'groups', api_views.GroupViewSet)
+router.register(r'users', api_views.UserProfileViewSet)
--- api/views.py
+++ api/views.py
@@ -1,19 +1,6 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from api.serializers import UserSerializer, GroupSerializer, \
-    UserProfileSerializer
+from api.serializers import UserProfileSerializer
 from main.models import UserProfile
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    lookup_field = 'username'
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
