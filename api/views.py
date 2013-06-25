ead53b58612a7f6f56b2045a1420b504425d1572
Ukang'a Dickson <ukanga@gmail.com>
2013-06-25 12:29:16 +0300

    added ObjectLookupMixin: provide correct filter for relations

--- api/views.py
+++ api/views.py
@@ -17,7 +17,26 @@ class UserViewSet(viewsets.ModelViewSet):
     lookup_field = 'username'
 
 
-class UserProfileViewSet(viewsets.ModelViewSet):
+class ObjectLookupMixin(object):
+    def get_object(self):
+        """
+        Incase the lookup is on an object that has been hyperlinked
+        then update the queryset filter appropriately
+        """
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(queryset)
+        filter = {}
+        serializer = self.get_serializer()
+        lookup_field = self.lookup_field
+        if self.lookup_field in serializer.get_fields():
+            k = serializer.get_fields()[self.lookup_field]
+            if isinstance(k, serializers.HyperlinkedRelatedField):
+                lookup_field = '%s__%s' % (self.lookup_field, k.lookup_field)
+        filter[lookup_field] = self.kwargs[self.lookup_field]
+        return get_object_or_404(queryset,  **filter)
+
+
+class UserProfileViewSet(ObjectLookupMixin, viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = 'user'
