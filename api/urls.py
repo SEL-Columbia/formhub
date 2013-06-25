990d2233a3d77a8326d3db151387f2ca9a13298a
Ukang'a Dickson <ukanga@gmail.com>
2013-06-25 12:27:42 +0300

    updated api urls to inculde u->User path

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from api import views as api_views
 
 router = routers.DefaultRouter()
 router.register(r'users', api_views.UserProfileViewSet)
+router.register(r'u', api_views.UserViewSet)
 router.register(r'forms', api_views.XFormViewSet)
 router.register(r'projects', api_views.ProjectViewSet)
