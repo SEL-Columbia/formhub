76fa074fb7e43036d90f59d4bd6bca5fe75ba02c
Ukang'a Dickson <ukanga@gmail.com>
2013-06-25 12:26:51 +0300

    UserViewSet added back

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
+from rest_framework import serializers, viewsets
 from api.serializers import UserProfileSerializer, XFormSerializer,\
-    ProjectSerializer
+    ProjectSerializer, UserSerializer
 from main.models import UserProfile
 
 from odk_logger.models import XForm
@@ -8,9 +11,16 @@ from odk_logger.models import XForm
 from api.models import Project
 
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'
+
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    lookup_field = 'user'
 
 
 class XFormViewSet(viewsets.ModelViewSet):
