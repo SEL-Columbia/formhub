15a011f854fc7723bae27d6f97bb899cdcbaab0e
Ukang'a Dickson <ukanga@gmail.com>
2013-06-28 15:19:37 +0300

    project view class with multi fields lookup

--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 
-from rest_framework import serializers, viewsets, mixins
-from api.serializers import UserProfileSerializer, XFormSerializer,\
-    ProjectSerializer, UserSerializer
+from rest_framework import viewsets
+from api import serializers as api_serializers
+from api import mixins
+
 from main.models import UserProfile
 
 from odk_logger.models import XForm
@@ -13,76 +13,23 @@ from api.models import Project
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = api_serializers.UserSerializer
     lookup_field = 'username'
 
 
-class ObjectLookupMixin(object):
-    def get_object(self):
-        """
-        Incase the lookup is on an object that has been hyperlinked
-        then update the queryset filter appropriately
-        """
-        queryset = self.get_queryset()
-        queryset = self.filter_queryset(queryset)
-        filter = {}
-        serializer = self.get_serializer()
-        lookup_field = self.lookup_field
-        if self.lookup_field in serializer.get_fields():
-            k = serializer.get_fields()[self.lookup_field]
-            if isinstance(k, serializers.HyperlinkedRelatedField):
-                lookup_field = '%s__%s' % (self.lookup_field, k.lookup_field)
-        filter[lookup_field] = self.kwargs[self.lookup_field]
-        return get_object_or_404(queryset,  **filter)
-
-    def pre_save(self, obj):
-        """
-        Set any attributes on the object that are implicit in the request.
-        """
-        # pk and/or slug attributes are implicit in the URL.
-        lookup = self.kwargs.get(self.lookup_field, None)
-        pk = self.kwargs.get(self.pk_url_kwarg, None)
-        slug = self.kwargs.get(self.slug_url_kwarg, None)
-        slug_field = slug and self.slug_field or None
-
-        if lookup:
-            serializer = self.get_serializer()
-            k = serializer.get_fields()[self.lookup_field]
-            queryset = self.get_queryset()
-            queryset = self.filter_queryset(queryset)
-            if isinstance(k, serializers.HyperlinkedRelatedField):
-                filter = {}
-                lookup_field = '%s__%s' % (self.lookup_field, k.lookup_field)
-                filter[lookup_field] = lookup
-                k_obj = get_object_or_404(queryset, **filter)
-                lookup = getattr(k_obj, self.lookup_field)
-            setattr(obj, self.lookup_field, lookup)
-
-        if pk:
-            setattr(obj, 'pk', pk)
-
-        if slug:
-            setattr(obj, slug_field, slug)
-
-        # Ensure we clean the attributes so that we don't eg return integer
-        # pk using a string representation, as provided by the url conf kwarg.
-        if hasattr(obj, 'full_clean'):
-            exclude = mixins._get_validation_exclusions(
-                obj, pk, slug_field, self.lookup_field)
-            obj.full_clean(exclude)
-
-
-class UserProfileViewSet(ObjectLookupMixin, viewsets.ModelViewSet):
+class UserProfileViewSet(mixins.ObjectLookupMixin, viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+    serializer_class = api_serializers.UserProfileSerializer
     lookup_field = 'user'
 
 
 class XFormViewSet(viewsets.ModelViewSet):
     queryset = XForm.objects.all()
-    serializer_class = XFormSerializer
+    serializer_class = api_serializers.XFormSerializer
 
 
-class ProjectViewSet(viewsets.ModelViewSet):
+class ProjectViewSet(mixins.MultiLookupMixin, viewsets.ModelViewSet):
     queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
+    serializer_class = api_serializers.ProjectSerializer
+    lookup_fields = ('owner', 'pk')
+    lookup_field = 'owner'
