3b2e845e782597dc5e42ef5e5c5dd12b6f6924f3
Ukang'a Dickson <ukanga@gmail.com>
2013-07-02 16:53:24 +0300

    changed import

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,15 +1,13 @@
 from django.contrib.auth.models import Permission
 from main.tests.test_base import MainTestCase
 from api.models import Team, OrganizationProfile, Project
-from api.utils import create_organization, add_user_to_team
-from api.utils import create_organization_team, add_team_to_project
-from api.utils import create_organization_project
+from api import utils
 
 
 class TestModels(MainTestCase):
     def test_create_organization_creates_team_and_perms(self):
         # create a user - bob
-        profile = create_organization("modilabs", self.user)
+        profile = utils.create_organization("modilabs", self.user)
         self.assertIsInstance(profile, OrganizationProfile)
         organization_profile = OrganizationProfile.objects.get(
             user__username="modilabs")
@@ -26,11 +24,11 @@ class TestModels(MainTestCase):
         self.assertTrue(self.user.has_perm('api.is_org_owner'))
 
     def test_create_organization_team(self):
-        profile = create_organization("modilabs", self.user)
+        profile = utils.create_organization("modilabs", self.user)
         organization = profile.user
         team_name = 'dev'
         perms = ['is_org_owner', ]
-        create_organization_team(organization, team_name, perms)
+        utils.create_organization_team(organization, team_name, perms)
         team_name = "modilabs#%s" % team_name
         dev_team = Team.objects.get(organization=organization, name=team_name)
         self.assertIsInstance(dev_team, Team)
@@ -38,7 +36,7 @@ class TestModels(MainTestCase):
             dev_team.permissions.get(codename='is_org_owner'), Permission)
 
     def _create_organization(self, org_name, user):
-        profile = create_organization(org_name, user)
+        profile = utils.create_organization(org_name, user)
         self.organization = profile.user
         return self.organization
 
@@ -49,12 +47,12 @@ class TestModels(MainTestCase):
 
         # create another team
         team_name = 'managers'
-        team = create_organization_team(organization, team_name)
-        add_user_to_team(team, user_deno)
+        team = utils.create_organization_team(organization, team_name)
+        utils.add_user_to_team(team, user_deno)
         self.assertIn(team.group_ptr, user_deno.groups.all())
 
     def _create_project(iself, organization, project_name, user):
-        project = create_organization_project(
+        project = utils.create_organization_project(
             organization, project_name, user)
         return project
 
@@ -66,7 +64,7 @@ class TestModels(MainTestCase):
         self.assertEqual(project.name, project_name)
 
         user_deno = self._create_user('deno', 'deno')
-        project = create_organization_project(
+        project = utils.create_organization_project(
             organization, project_name, user_deno)
         self.assertIsNone(project)
 
@@ -75,7 +73,7 @@ class TestModels(MainTestCase):
         project_name = "demo"
         team_name = "enumerators"
         project = self._create_project(organization, project_name, self.user)
-        team = create_organization_team(organization, team_name)
-        result = add_team_to_project(team, project)
+        team = utils.create_organization_team(organization, team_name)
+        result = utils.add_team_to_project(team, project)
         self.assertTrue(result)
         self.assertIn(project, team.projects.all())
