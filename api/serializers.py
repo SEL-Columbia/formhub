d54d803b18bdb224185fdf6ee874de9ad2f0ed39
Ukang'a Dickson <ukanga@gmail.com>
2013-07-08 13:59:28 +0300

    serialize projects belonging to a team

--- api/serializers.py
+++ api/serializers.py
@@ -11,7 +11,8 @@ from main.forms import UserProfileForm, RegistrationFormUserProfile
 from odk_logger.models import XForm
 
 from api.models import Project, OrganizationProfile, Team
-from api.fields import HyperlinkedMultiIdentityField
+from api.fields import HyperlinkedMultiIdentityField,\
+    HyperlinkedMultiRelatedField
 
 from api import utils
 
@@ -183,12 +184,20 @@ class TeamSerializer(serializers.Serializer):
         source='organization',
         queryset=User.objects.filter(
             pk__in=OrganizationProfile.objects.values('user')))
+    projects = HyperlinkedMultiRelatedField(
+        view_name='project-detail', source='projects', many=True,
+        queryset=Project.objects.all(), read_only=True,
+        lookup_fields=(('pk', 'pk'), ('owner', 'organization')))
 
     def restore_object(self, attrs, instance=None):
         org = attrs.get('organization', None)
+        projects = attrs.get('projects', [])
         if instance:
             instance.organization = org if org else instance.organization
             instance.name = attrs.get('team_name', instance.name)
+            instance.projects.clear()
+            for project in projects:
+                instance.projects.add(project)
             return instance
         team_name = attrs.get('team_name', None)
         if not team_name:
