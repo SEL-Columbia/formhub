13e87e7dee23581ef550dd174d5275a87b8500f8
Ukang'a Dickson <ukanga@gmail.com>
2013-07-08 12:30:02 +0300

    Team api viewset

--- api/views.py
+++ api/views.py
@@ -13,7 +13,7 @@ from main.models import UserProfile
 
 from odk_logger.models import XForm
 
-from api.models import Project, OrganizationProfile, ProjectXForm
+from api.models import Project, OrganizationProfile, ProjectXForm, Team
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -85,4 +85,29 @@ class ProjectViewSet(mixins.MultiLookupMixin, viewsets.ModelViewSet):
             qs = get_object_or_404(ProjectXForm, **filter)
             data = qs.xform
         serializer = api_serializers.XFormSerializer(data, many=many)
+        return Response(serializer.data)
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = api_serializers.TeamSerializer
+    lookup_fields = ('owner', 'pk')
+    lookup_field = 'owner'
+    extra_lookup_fields = None
+
+    def get_object(self):
+        filter = {
+            'organization__username': self.kwargs['owner'],
+            'pk': self.kwargs['pk']
+        }
+        qs = self.filter_queryset(self.get_queryset())
+        return get_object_or_404(qs, **filter)
+
+    def list(self, request, **kwargs):
+        filter = {}
+        if 'owner' in kwargs:
+            filter['organization__username'] = kwargs['owner']
+        qs = self.filter_queryset(self.get_queryset())
+        self.object_list = qs.filter(**filter)
+        serializer = self.get_serializer(self.object_list, many=True)
         return Response(serializer.data)
