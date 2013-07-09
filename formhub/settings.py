14103ac0a4d3329d5f8eaa882aabf22d1674be1b
Ukang'a Dickson <ukanga@gmail.com>
2013-07-09 18:20:02 +0300

    prevent admin error emails on DEBUG False - see https://docs.djangoproject.com/en/dev/releases/1.4/#request-exceptions-are-now-always-logged

--- formhub/settings.py
+++ formhub/settings.py
@@ -205,9 +205,15 @@ LOGGING = {
             'format': '%(levelname)s %(message)s'
         },
     },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
         'console': {
