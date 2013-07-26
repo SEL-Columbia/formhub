53c844741eb1927c16d16aa49ee7b7722c686aa9
Ukang'a Dickson <ukanga@gmail.com>
2013-07-26 14:34:15 +0300

    change rest permission for docs to work even when logged out

--- formhub/settings.py
+++ formhub/settings.py
@@ -185,10 +185,10 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     )
