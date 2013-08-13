67395b211a21daf64dd8eac21c1bad00903219d0
Ukang'a Dickson <ukanga@gmail.com>
2013-08-13 17:10:38 +0300

    test /api/v1/forms/{owner}/{formid}/form.(xml|json)

--- api/tests/test_forms_api.py
+++ api/tests/test_forms_api.py
@@ -1,3 +1,8 @@
+import os
+
+from xml.dom import minidom, Node
+from django.conf import settings
+
 from api.tests.test_api import TestAPICase
 from api.views import XFormViewSet
 
@@ -30,3 +35,47 @@ class TestFormsAPI(TestAPICase):
         response = view(request, owner='bob', pk=1)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, self.form_data)
+
+    def test_form_format(self):
+        self._publish_xls_form_to_project()
+        view = XFormViewSet.as_view({
+            'get': 'form'
+        })
+        data = {
+            "name": "transportation",
+            "title": "transportation_2011_07_25",
+            "default_language": "default",
+            "id_string": "transportation_2011_07_25",
+            "type": "survey",
+        }
+        request = self.factory.get('/', **self.extra)
+        response = view(request, owner='bob', pk=1, formid=1, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertDictContainsSubset(data, response.data)
+        response = view(request, owner='bob', pk=1, formid=1, format='xml')
+        self.assertEqual(response.status_code, 200)
+        response_doc = minidom.parseString(response.data)
+
+        xml_path = os.path.join(
+            settings.PROJECT_ROOT, "main", "tests", "fixtures",
+            "transportation", "transportation.xml")
+        with open(xml_path) as xml_file:
+            expected_doc = minidom.parse(xml_file)
+
+        model_node = [
+            n for n in
+            response_doc.getElementsByTagName("h:head")[0].childNodes
+            if n.nodeType == Node.ELEMENT_NODE and
+            n.tagName == "model"][0]
+
+        # check for UUID and remove
+        uuid_nodes = [
+            node for node in model_node.childNodes
+            if node.nodeType == Node.ELEMENT_NODE
+            and node.getAttribute("nodeset") == "/transportation/formhub/uuid"]
+        self.assertEqual(len(uuid_nodes), 1)
+        uuid_node = uuid_nodes[0]
+        uuid_node.setAttribute("calculate", "''")
+
+        # check content without UUID
+        self.assertEqual(response_doc.toxml(), expected_doc.toxml())
