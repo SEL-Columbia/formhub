a637c54c1ded890bfa5f0e7eba69e93c98803321
Ukang'a Dickson <ukanga@gmail.com>
2013-08-14 13:24:52 +0300

    use https for ENKETO_URL

--- formhub/settings.py
+++ formhub/settings.py
@@ -80,7 +80,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 #ENKETO URL
-ENKETO_URL = 'http://enketo.formhub.org/'
+ENKETO_URL = 'https://enketo.formhub.org/'
 ENKETO_API_SURVEY_PATH = '/api_v1/survey'
 ENKETO_API_INSTANCE_PATH = '/api_v1/instance'
 ENKETO_PREVIEW_URL = ENKETO_URL + 'webform/preview'
