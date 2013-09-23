50af7f4a090718e1f68b243a90ac6996c60a53b0
vernondcole <vernon.cole@ehealthnigeria.org>
2013-09-23 09:57:25 +0100

    submission-time is an adjective phrase describing 'validations'. Refactoring to make that clearer.

--- odk_logger/validations.py
+++ odk_logger/validations.py
@@ -1,15 +1,13 @@
 """
-formhub/odk_logger/validations.py --> Defines user-written validation routines for various ODK X-forms.
-   (works kind of like urls.py)
+validations.py --> Defines Submission-time Validation routines for various ODK X-forms.   (works kind of like urls.py)
 
-This file must define an object named "validation_patterns",
- which must contain either: an empty sequence,
- or the value returned from a call to submission_time_validation.val_patterns(),
-   which takes as arguments a sequence of 2-tuples,
-    each of which was returned from the function submission_time_validation.val(),
-    which takes two arguments:
-      1: a Python regular expression matching the name of the X-form to be validated
-      2: a Python callable (i.e. function) which is used to validate one row of data from said form.
+This file must define a variable named "validation_patterns",
+which is the value returned from a call to submission_time_validation.val_patterns(),
+which takes as arguments a sequence of 2-tuples,
+each of which is the return from the function submission_time_validation.val(),
+which takes two arguments:
+1: a Python regular expression matching the name of the X-form to be validated
+2: a Python callable (i.e. function) which is used to validate one row of data from said form.
 - - - - - -
 said function will be called with an argument list like the following:
 def dummy_callable(form_name, xml_root, request, username, uuid, media_files):
@@ -20,8 +18,7 @@ def dummy_callable(form_name, xml_root, request, username, uuid, media_files):
  or return True to inhibit record loading with a success indication
  or return an utils.logger_tools.OpenRosaResponseNotAcceptable Exception to inhibit record loading with a message
 """
-# - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-from utils.submission_time_validation import val_patterns, val, dummy_callable
+from utils.submissionTime_validation import val_patterns, val, dummy_callable
 
 # validation_patterns = []  ## if extended validations are not used, set this to an empty list
 
