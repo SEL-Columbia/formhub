b8501b21df6d1e78c1a4789ca158c07ee9dace40
vernondcole <vernon.cole@ehealthnigeria.org>
2013-09-23 10:01:16 +0100

    comment correction

--- odk_logger/validations.py
+++ odk_logger/validations.py
@@ -1,10 +1,14 @@
 """
 validations.py --> Defines Submission-time Validation routines for various ODK X-forms.   (works kind of like urls.py)
 
+This table is called at submission time (i.e.: when a user "submits" an X-form to the formhub site)
+and searched to see if the present form needs extended validation.
+If the form name matches a regular expression, the matching callback function is executed.
+
 This file must define a variable named "validation_patterns",
-which is the value returned from a call to submission_time_validation.val_patterns(),
+which is the value returned from a call to submissionTime_validation.val_patterns(),
 which takes as arguments a sequence of 2-tuples,
-each of which is the return from the function submission_time_validation.val(),
+each of which is the return from the function submissionTime_validation.val(),
 which takes two arguments:
 1: a Python regular expression matching the name of the X-form to be validated
 2: a Python callable (i.e. function) which is used to validate one row of data from said form.
