64614a06feaa63ed46f37a511eb2ffedc84444e4
Ukang'a Dickson <ukanga@gmail.com>
2013-10-01 11:02:29 +0300

    add DjangoModelPermissions as one of default DjangoModelPermissions

--- formhub/settings.py
+++ formhub/settings.py
@@ -190,7 +190,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.DjangoModelPermissions'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.BasicAuthentication',
