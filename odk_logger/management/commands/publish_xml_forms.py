e9c0db8366a83be0be7e5cb14a5dee83a6051183
vernondcole <vernon.cole@ehealthnigeria.org>
2013-11-16 12:38:29 +0100

    new publish_xml_forms command

--- odk_logger/management/commands/publish_xml_forms.py
+++ odk_logger/management/commands/publish_xml_forms.py
@@ -3,10 +3,13 @@
 
 import os, glob
 from optparse import make_option
-from django.core.management.base import BaseCommand
+
+from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import ugettext_lazy
-from odk_logger.models.xform import XForm
 from django.contrib.auth.models import User
+from pyxform.xform2json import create_survey_element_from_xml
+
+from odk_logger.models.xform import XForm
 from odk_logger.models.instance import get_id_string_from_xml_str
 
 
@@ -20,18 +23,18 @@ class Command(BaseCommand):
             help=ugettext_lazy("Replace existing form if any")),
         )
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         path = args[0]
 
         try:
             username = args[1]
         except IndexError:
-            raise CommandError(_("You must provide the username to publish the form to."))
+            raise CommandError("You must provide the username to publish the forms to.")
         # make sure user exists
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
-            raise CommandError(_("The user '%s' does not exist.") % username)
+            raise CommandError("The user '{}' does not exist.".format(username))
 
         for form in glob.glob( os.path.join(path, "*") ):
             f = open(form)
@@ -44,7 +47,11 @@ class Command(BaseCommand):
 
             if form_already_exists:
                 if options.has_key('replace') and options['replace']:
-                    XForm.objects.filter(user=user,id_string=id_string).delete()
-
-            XForm.objects.get_or_create(xml=xml, downloadable=False, user=user, id_string=id_string)
+                    XForm.objects.filter(user=user, id_string=id_string).delete()
+                else:
+                    raise CommandError('form "{}" is already defined, and --replace was not specified.'.format(
+                        id_string))
+            survey = create_survey_element_from_xml(xml)
+            form_json = survey.to_json()
+            XForm.objects.get_or_create(xml=xml, downloadable=True, user=user, id_string=id_string, json=form_json)
             f.close()
